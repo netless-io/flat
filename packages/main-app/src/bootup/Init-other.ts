88c1570ac39f29c0d44b792e1edea5dc19f1b85a
Black-Hole <158blackhole@gmail.com>
2020-08-29 14:41:19 +0800

    fix(electron): turn off annoying warnings in the low version of electron

---
+++ packages/main-app/src/bootup/Init-other.ts
@@ -0,0 +1,22 @@
+import { session } from "electron";
+
+export default () => {
+    // fix electron bug
+    // turn off annoying warnings
+    // see: https://github.com/electron/electron/issues/13008#issuecomment-575909942
+    const redirectURL =
+        "data:application/x-javascript;base64,UHJvZHVjdFJlZ2lzdHJ5SW1wbC5SZWdpc3RyeT1jbGFzc3tjb25zdHJ1Y3Rvcigpe31uYW1lRm9yVXJsKHIpe3JldHVybiBudWxsfWVudHJ5Rm9yVXJsKHIpe3JldHVybiBudWxsfXR5cGVGb3JVcmwocil7cmV0dXJuIG51bGx9fSxQcm9kdWN0UmVnaXN0cnlJbXBsLl9oYXNoRm9yRG9tYWluPWZ1bmN0aW9uKHIpe3JldHVybiIifSxQcm9kdWN0UmVnaXN0cnlJbXBsLnJlZ2lzdGVyPWZ1bmN0aW9uKHIsdCl7UHJvZHVjdFJlZ2lzdHJ5SW1wbC5fcHJvZHVjdHNCeURvbWFpbkhhc2g9bmV3IE1hcH0sUHJvZHVjdFJlZ2lzdHJ5SW1wbC5fcHJvZHVjdHNCeURvbWFpbkhhc2g9bmV3IE1hcCxQcm9kdWN0UmVnaXN0cnlJbXBsLnJlZ2lzdGVyKFtdLFtdKSxQcm9kdWN0UmVnaXN0cnlJbXBsLnNoYTE9ZnVuY3Rpb24ocil7cmV0dXJuIiJ9Ow==";
+    session.defaultSession.webRequest.onBeforeRequest((details, callback) => {
+        if (
+            /^devtools:\/\/devtools\/remote\/serve_file\/@[0-9a-f]{40}\/product_registry_impl\/product_registry_impl_module.js$/iu.test(
+                details.url,
+            )
+        ) {
+            callback({
+                redirectURL,
+            });
+            return;
+        }
+        callback({});
+    });
+};
--- packages/main-app/src/index.ts
+++ packages/main-app/src/index.ts
@@ -4,5 +4,13 @@ import initWindow from "./bootup/Init-window";
 import initMenus from "./bootup/Init-menus";
 import intIPC from "./bootup/Init-ipc";
 import initAppListen from "./bootup/Init-app-listener";
+import initOtherListeners from "./bootup/Init-other";
 
-void bootstrap({} as Context, [intEnv, initWindow, initMenus, intIPC, initAppListen]);
+void bootstrap({} as Context, [
+    intEnv,
+    initWindow,
+    initMenus,
+    intIPC,
+    initAppListen,
+    initOtherListeners,
+]);
