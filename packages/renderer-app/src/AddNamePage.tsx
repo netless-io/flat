578c71d2f3879a82c7d9c2d971192c2d9eb9243f
rick <rick@netless.link>
2020-09-01 15:38:25 +0800

    fix ui  bug

--- packages/renderer-app/src/AddNamePage.tsx
+++ packages/renderer-app/src/AddNamePage.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {RouteComponentProps} from "react-router";
-import {Link} from "react-router-dom";
+import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
 import "./AddNamePage.less";
 import logo from "./assets/image/logo.svg";
-import {Button, Input} from "antd";
+import { Button, Input } from "antd";
 
 export type JoinPageStates = {
     name: string;
@@ -18,41 +18,42 @@ export default class AddNamePage extends React.Component<RouteComponentProps<{}>
     }
 
     private handleJoin = (): void => {
-        const {name} = this.state;
+        const { name } = this.state;
         localStorage.setItem("userName", name);
         this.props.history.push(`/whiteboard/`);
-    }
+    };
 
     public render(): React.ReactNode {
-        const {name} = this.state;
+        const { name } = this.state;
         return (
             <div className="page-index-box">
                 <div className="page-index-mid-box">
                     <div className="page-index-logo-box">
-                        <img src={logo}/>
-                        <span>
-                                0.0.1
-                        </span>
+                        <img src={logo} />
+                        <span>0.0.1</span>
                     </div>
                     <div className="page-index-form-box">
-                        <Input placeholder={"输入昵称"}
-                               maxLength={8}
-                               value={name}
-                               onChange={evt => this.setState({name: evt.target.value})}
-                               className="page-index-input-box"
-                               size={"large"}/>
+                        <Input
+                            placeholder={"输入昵称"}
+                            maxLength={8}
+                            value={name}
+                            onChange={evt => this.setState({ name: evt.target.value })}
+                            style={{ width: 384, marginBottom: 28 }}
+                            size={"large"}
+                        />
                         <div className="page-index-btn-box">
                             <Link to={"/"}>
-                                <Button className="page-index-btn"
-                                        size={"large"}>
+                                <Button className="page-index-btn" size={"large"}>
                                     返回首页
                                 </Button>
                             </Link>
-                            <Button className="page-index-btn"
-                                    disabled={name === ""}
-                                    size={"large"}
-                                    onClick={this.handleJoin}
-                                    type={"primary"}>
+                            <Button
+                                className="page-index-btn"
+                                disabled={name === ""}
+                                size={"large"}
+                                onClick={this.handleJoin}
+                                type={"primary"}
+                            >
                                 创建房间
                             </Button>
                         </div>
@@ -62,4 +63,3 @@ export default class AddNamePage extends React.Component<RouteComponentProps<{}>
         );
     }
 }
-
--- packages/renderer-app/src/JoinPage.tsx
+++ packages/renderer-app/src/JoinPage.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {RouteComponentProps} from "react-router";
+import { RouteComponentProps } from "react-router";
 import "./JoinPage.less";
 import logo from "./assets/image/logo.svg";
-import {Button, Input} from "antd";
-import {Link} from "react-router-dom";
+import { Button, Input } from "antd";
+import { Link } from "react-router-dom";
 
 export type JoinPageStates = {
     roomId: string;
@@ -26,42 +26,45 @@ export default class JoinPage extends React.Component<RouteComponentProps<{}>, J
             localStorage.setItem("userName", this.state.name);
         }
         this.props.history.push(`/whiteboard/${this.state.roomId}/${userId}/`);
-    }
+    };
 
     public render(): React.ReactNode {
-        const {roomId, name} = this.state;
+        const { roomId, name } = this.state;
         return (
             <div className="page-index-box">
                 <div className="page-index-mid-box">
                     <div className="page-index-logo-box">
-                        <img src={logo}/>
-                        <span>
-                                0.0.1
-                        </span>
+                        <img src={logo} />
+                        <span>0.0.1</span>
                     </div>
                     <div className="page-index-form-box">
-                        <Input placeholder={"输入名字"}
-                               value={name}
-                               onChange={evt => this.setState({name: evt.target.value})}
-                               className="page-index-input-box"
-                               size={"large"}/>
-                        <Input placeholder={"输入房间号"}
-                               value={roomId}
-                               onChange={evt => this.setState({roomId: evt.target.value})}
-                               className="page-index-input-box"
-                               size={"large"}/>
+                        <Input
+                            placeholder={"输入名字"}
+                            value={name}
+                            onChange={evt => this.setState({ name: evt.target.value })}
+                            style={{ width: 384, marginBottom: 28 }}
+                            size={"large"}
+                        />
+                        <Input
+                            placeholder={"输入房间号"}
+                            value={roomId}
+                            onChange={evt => this.setState({ roomId: evt.target.value })}
+                            style={{ width: 384, marginBottom: 28 }}
+                            size={"large"}
+                        />
                         <div className="page-index-btn-box">
                             <Link to={"/"}>
-                                <Button className="page-index-btn"
-                                        size={"large"}>
+                                <Button className="page-index-btn" size={"large"}>
                                     返回首页
                                 </Button>
                             </Link>
-                            <Button className="page-index-btn"
-                                    disabled={roomId === "" || name === ""}
-                                    size={"large"}
-                                    onClick={this.handleJoin}
-                                    type={"primary"}>
+                            <Button
+                                className="page-index-btn"
+                                disabled={roomId === "" || name === ""}
+                                size={"large"}
+                                onClick={this.handleJoin}
+                                type={"primary"}
+                            >
                                 加入房间
                             </Button>
                         </div>
@@ -71,4 +74,3 @@ export default class JoinPage extends React.Component<RouteComponentProps<{}>, J
         );
     }
 }
-
