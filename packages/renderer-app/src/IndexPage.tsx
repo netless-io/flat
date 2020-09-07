30bc62ba8f3fbf9b74ef96951957584d8b9b965d
rick <rick@netless.link>
2020-09-07 09:14:22 +0800

    fix url bug

--- packages/renderer-app/src/IndexPage.tsx
+++ packages/renderer-app/src/IndexPage.tsx
@@ -6,7 +6,7 @@ import join from "./assets/image/join.svg";
 import create from "./assets/image/create.svg";
 import "./IndexPage.less";
 import { Button, Input, Popover } from "antd";
-import { ipcRenderer } from "electron";
+import { ipcRenderer, shell } from "electron";
 
 export type IndexPageStates = {
     name: string;
@@ -51,6 +51,18 @@ class IndexPage extends React.Component<RouteComponentProps<{}>, IndexPageStates
             localStorage.setItem("userName", this.state.name);
             this.setState({ visible: false });
         }
+    };
+
+    private openGithub = async (): Promise<void> => {
+        await shell.openExternal("https://github.com/netless-io/Flat-native");
+    };
+
+    private openLanding = async (): Promise<void> => {
+        await shell.openExternal("https://netless.link/");
+    };
+
+    private openMIT = async (): Promise<void> => {
+        await shell.openExternal("https://opensource.org/licenses/MIT");
     };
     public render(): React.ReactNode {
         return (
@@ -104,39 +116,30 @@ class IndexPage extends React.Component<RouteComponentProps<{}>, IndexPageStates
                     <div className="page-index-start-box">
                         <div className="page-index-start-cell">
                             <Link to={"/join/"}>
-                                <img src={join} />
+                                <img src={join} alt={"join"} />
                             </Link>
                             <span>加入房间</span>
                         </div>
                         <div className="page-cutline-box" />
                         <div className="page-index-start-cell">
                             <div onClick={this.handleCreate}>
-                                <img src={create} />
+                                <img src={create} alt={"create"} />
                             </div>
                             <span>创建房间</span>
                         </div>
                     </div>
                     <div className="page-index-link-box">
                         <div className="page-index-cell-left">
-                            <a href={"https://netless.link/"} target={"_blank"}>
-                                官网
-                            </a>
+                            <span onClick={this.openLanding}>官网</span>
                         </div>
                         <div className="page-cutline-link-box" />
                         <div className="page-index-cell-right">
-                            <a
-                                href={"https://github.com/netless-io/react-whiteboard"}
-                                target={"_blank"}
-                            >
-                                Github
-                            </a>
+                            <span onClick={this.openGithub}>Github</span>
                         </div>
                     </div>
                     <div className="page-index-start-term">
                         本开源项目遵循
-                        <a href={"https://opensource.org/licenses/MIT"} target={"_blank"}>
-                            《 MIT 开源协议》
-                        </a>
+                        <span onClick={this.openMIT}>《 MIT 开源协议》</span>
                     </div>
                 </div>
             </div>
