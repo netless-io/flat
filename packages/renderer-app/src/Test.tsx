1f8f8c7ce5b9426d14cb7e4f1f161721268978e1
Black-Hole <158blackhole@gmail.com>
2020-09-13 10:12:22 +0800

    feat(renderer): add download file and unzip file utils function

--- packages/renderer-app/src/Test.tsx
+++ packages/renderer-app/src/Test.tsx
@@ -1,12 +1,75 @@
 import * as React from "react";
 import { Button } from "antd";
+import { DownloadFile } from "./utils/download";
+import { extractZIP } from "./utils/unzip";
+import { removeSync } from "fs-extra";
+import path from "path";
 
-export default class Test extends React.Component<{}, {}> {
+export default class Test extends React.Component<
+    {},
+    {
+        downloadProgress: number;
+    }
+> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            downloadProgress: 0,
+        };
+    }
+
+    private noticeDownloadZip = (url: string) => {
+        const download = new DownloadFile(url);
+
+        download.onProgress(p => {
+            this.setState({
+                downloadProgress: p.progress,
+            });
+        });
+
+        download.onEnd(d => {
+            console.log("下载完成");
+            console.log("开始解压");
+
+            extractZIP(d.filePath)
+                .then(() => {
+                    console.log("解压完成");
+
+                    // 解压成功后删除压缩文件
+                    removeSync(path.join(d.filePath));
+
+                    // 清空文件夹下的所有内容
+                    // emptyDirSync(path.dirname(d.filePath));
+                })
+                .catch(e => {
+                    console.log("解压失败");
+                    console.log(e);
+                });
+        });
+
+        download.onError(e => {
+            console.error("下载失败");
+            console.error(e);
+        });
+
+        console.log(`文件是否已经存在: ${download.fileIsExists("test.zip")}`);
+
+        download.start();
+    };
+
     public render(): React.ReactNode {
         return (
             <div className="page-index-box">
-                <Button>点击下载</Button>
-                <div>下载百分比 100%</div>
+                <Button
+                    onClick={() =>
+                        this.noticeDownloadZip(
+                            "https://white-sdk.oss-cn-beijing.aliyuncs.com/images/test.zip",
+                        )
+                    }
+                >
+                    点击下载
+                </Button>
+                <div>下载百分比: {this.state.downloadProgress}%</div>
                 https://white-sdk.oss-cn-beijing.aliyuncs.com/images/test.zip
             </div>
         );
---
+++ packages/renderer-app/src/utils/unzip.ts
@@ -0,0 +1,21 @@
+import extract from "extract-zip";
+import path from "path";
+
+/**
+ * 解压 zip 文件
+ * @param {string} filePath - zip 文件绝对路径
+ * @param {string} extractDir - 解压的目录，默认为和 zip 同目录下的同名文件夹，如:(test.zip -> test/)
+ * @return {Promise<void>}
+ */
+export const extractZIP = (filePath: string, extractDir?: string): Promise<void> => {
+    let dir = "";
+    if (extractDir) {
+        dir = extractDir;
+    } else {
+        dir = path.join(path.dirname(filePath), path.basename(filePath).replace(".zip", ""));
+    }
+
+    return extract(filePath, {
+        dir,
+    });
+};
