0c3c9cc9513f4b20a0d9361c1d043bf09cc1ec7f
crimx <straybugs@gmail.com>
2020-11-10 18:59:39 +0800

    refactor(cloudrecording): add CloudingRecording

--- src/renderer-app/typings/custom.d.ts
+++ src/renderer-app/typings/custom.d.ts
@@ -8,14 +8,22 @@ declare namespace NodeJS {
         NODE_ENV: "development" | "production";
         NETLESS_SDK_TOKEN: string;
         NETLESS_APP_IDENTIFIER: string;
+
         OSS_ACCESS_KEY_ID: string;
         OSS_ACCESS_KEY_SECRET: string;
         OSS_REGION: string;
         OSS_BUCKET: string;
         OSS_FOLDER: string;
         OSS_PREFIX: string;
+
         AGORA_APP_ID: string;
         AGORA_RESTFUL_ID: string;
         AGORA_RESTFUL_SECRET: string;
+        AGORA_OSS_ACCESS_KEY_ID: string;
+        AGORA_OSS_ACCESS_KEY_SECRET: string;
+        AGORA_OSS_REGION: string;
+        AGORA_OSS_BUCKET: string;
+        AGORA_OSS_FOLDER: string;
+        AGORA_OSS_PREFIX: string;
     }
 }
