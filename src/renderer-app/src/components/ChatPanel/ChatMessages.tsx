d3045976b2111d5ad45cb4c43abe6c95936ac394
crimx <straybugs@gmail.com>
2020-12-14 00:27:07 +0800

    refactor: add allow raise hands button

--- src/renderer-app/src/components/ChatPanel/ChatMessages.tsx
+++ src/renderer-app/src/components/ChatPanel/ChatMessages.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ChatTypeBox, ChatTypeBoxProps } from "./ChatTypeBox";
 import { RTMessage } from "./ChatMessage";
 import { ChatMessageList, OnLoadMore } from "./ChatMessageList";
+import noHand from "../../assets/image/no-hand.svg";
 import "./ChatMessages.less";
 
 export interface ChatMessagesProps {
@@ -15,19 +16,26 @@ export interface ChatMessagesProps {
 export interface ChatMessageState {
     isBan: boolean;
     isRaiseHand: boolean;
+    isAllowRaiseHand: boolean;
 }
 
 export class ChatMessages extends React.PureComponent<ChatMessagesProps, ChatMessageState> {
     state = {
         isBan: false,
         isRaiseHand: false,
+        isAllowRaiseHand: true,
     };
     // @TODO 实现禁言功能，需要后端配合
     toogleBan = () => {
         this.setState(state => ({ isBan: !state.isBan }));
     };
 
-    // @TODO 实现举手功能
+    // @TODO 实现老师端举手功能
+    toggleAllowRaiseHand = () => {
+        this.setState(state => ({ isAllowRaiseHand: !state.isAllowRaiseHand }));
+    };
+
+    // @TODO 实现学生端举手功能
     toggleRaiseHand = () => {
         this.setState(state => ({ isRaiseHand: !state.isRaiseHand }));
     };
@@ -38,8 +46,20 @@ export class ChatMessages extends React.PureComponent<ChatMessagesProps, ChatMes
 
     render(): React.ReactNode {
         const { identity, userId, messages, onMessageSend, onLoadMore } = this.props;
+        const { isAllowRaiseHand } = this.state;
         return (
             <div className="chat-messages-wrap">
+                {isAllowRaiseHand && (
+                    <div className="chat-messages-cancel-hands-wrap">
+                        <button
+                            className="chat-messages-cancel-hands"
+                            onClick={this.toggleAllowRaiseHand}
+                        >
+                            <img src={noHand} alt="cancel hand raising" />
+                            取消举手
+                        </button>
+                    </div>
+                )}
                 <div className="chat-messages">
                     {messages.length > 0 ? (
                         <div className="chat-messages-box">
