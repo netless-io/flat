476435bd35b23942788485753493df9648670382
cheerchen <meetcheerego@gmail.com>
2020-12-18 13:54:15 +0800

    feat: add part of the UI to SettingPanel's components

--- src/renderer-app/src/components/UserSettingPanel/FileSetting.tsx
+++ src/renderer-app/src/components/UserSettingPanel/FileSetting.tsx
@@ -1,14 +1,24 @@
 import React from "react"
+import Checkbox from "antd/lib/checkbox/Checkbox"
+import "./FileSetting.less"
+import { Input, Button } from "antd"
 
 export default class FileSetting extends React.PureComponent<{}> {
     public render() {
         return (
             <div className="content-container">
                 <div className="header-container">
-                    文件设置
+                    <span>文件设置</span>
                 </div>
-                <div className="content-inner">
-                    内容
+                <div className="cache-container">
+                    <div className="inner-cache">
+                    <Checkbox>缓存所有课件</Checkbox>
+                        <p>把缓存文件保存至</p>
+                        <Input type="file"></Input> 
+                        <Button>打开文件夹</Button>
+                    </div>
+                        <p>目前有 77.77 MB 缓存文件</p>
+                        <Button>清理缓存</Button>
                 </div>
             </div>
         )
--- src/renderer-app/src/components/UserSettingPanel/MicrophoneTesting.tsx
+++ src/renderer-app/src/components/UserSettingPanel/MicrophoneTesting.tsx
@@ -1,14 +1,34 @@
 import React from "react"
+import { Select, Slider, Button } from "antd"
+import { Link } from "react-router-dom";
 
 export default class MicrophoneTesting extends React.PureComponent<{}> {
     public render() {
+        const { Option } = Select;
         return (
             <div className="content-container">
                 <div className="header-container">
-                    麦克风检测
+                    <span>麦克风检测</span>
                 </div>
-                <div className="content-inner">
-                    内容
+                <div className="speaker-testing-container">
+                    <p>麦克风</p>
+                    <Select defaultValue="MacBook Pro 麦克风">
+                        <Option value="cheerchen">MacBook Pro 麦克风</Option>
+                    </Select>
+                    {/* TODO  MicrophoneTesting*/}
+                    <p>试听声音</p>
+                    <div className="speaker-audio-testing">
+                        <p>音量</p>
+                        <Slider defaultValue={30}></Slider>
+                    </div>
+                    <div className="testing-btn">
+                        <Button>
+                            <Link to="/setting/microphone/">不可以听到</Link>
+                        </Button>
+                        <Button type="primary">
+                            <Link to="/setting/microphone/">可以听到</Link>
+                        </Button>
+                    </div>
                 </div>
             </div>
         )
