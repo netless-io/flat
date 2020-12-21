a4ac5a2367c11b7301081bde6de160e5907291a5
伍双 <beingwu@gmail.com>
2020-12-21 17:07:01 +0800

    add nav avatar

--- src/renderer-app/src/components/MainMenu.tsx
+++ src/renderer-app/src/components/MainMenu.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import { Menu } from "antd";
-import home from "../assets/image/home.svg"
-import homeActive from "../assets/image/home-active.svg"
-import user from "../assets/image/user.svg"
-import userActive from "../assets/image/user-active.svg"
-import setting from "../assets/image/setting.svg"
-import settingActive from "../assets/image/setting-active.svg"
+import home from "../assets/image/home.svg";
+import homeActive from "../assets/image/home-active.svg";
+import user from "../assets/image/user.svg";
+import userActive from "../assets/image/user-active.svg";
+import setting from "../assets/image/setting.svg";
+import settingActive from "../assets/image/setting-active.svg";
 import { Link } from "react-router-dom";
 
 export enum MainMenuKey {
     InfoPath = "/user/",
     MyPath = "/info/",
-    SettingPath = "/setting/"
+    SettingPath = "/setting/",
 }
 
-export class MainMenu extends React.PureComponent<{}> {
+export class MainMenu extends PureComponent<{}> {
     render() {
         let key = "";
         const hash = window.location.hash.substring(1);
@@ -25,25 +25,36 @@ export class MainMenu extends React.PureComponent<{}> {
         } else if (/^\/setting/.test(hash)) {
             key = "/setting/";
         }
-        
+
         return (
             <Menu className="menu-container" defaultSelectedKeys={[key]}>
-                <Menu.Item icon={<img src={key === "/user/" ? homeActive : home} />} key={MainMenuKey.InfoPath}>
+                <Menu.Item
+                    icon={<img src={key === "/user/" ? homeActive : home} alt={"home"} />}
+                    key={MainMenuKey.InfoPath}
+                >
                     <Link to="/user/">
                         <span>首页</span>
-                    </Link>    
+                    </Link>
                 </Menu.Item>
-                <Menu.Item icon={<img src={key === "/info/" ? userActive : user} />} key={MainMenuKey.MyPath}>
+                <Menu.Item
+                    icon={<img src={key === "/info/" ? userActive : user} alt={"user"} />}
+                    key={MainMenuKey.MyPath}
+                >
                     <Link to="/info/">
                         <span>我的</span>
                     </Link>
                 </Menu.Item>
-                <Menu.Item icon={<img src={key === "/setting/" ? settingActive : setting} />} key={MainMenuKey.SettingPath}>
+                <Menu.Item
+                    icon={
+                        <img src={key === "/setting/" ? settingActive : setting} alt={"setting"} />
+                    }
+                    key={MainMenuKey.SettingPath}
+                >
                     <Link to="/setting/normal/">
                         <span>设置</span>
                     </Link>
                 </Menu.Item>
             </Menu>
-        )
+        );
     }
 }
