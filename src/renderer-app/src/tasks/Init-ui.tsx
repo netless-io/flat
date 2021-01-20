1d8b6d6da6da1ead9a88bcf14f10fcafd0f528ae
crimx <straybugs@gmail.com>
2021-01-20 16:06:27 +0800

    chore: add mobx and hooks

--- src/renderer-app/src/tasks/Init-ui.tsx
+++ src/renderer-app/src/tasks/Init-ui.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.less";
 import "../theme.less";
 import { AppRoutes } from "../AppRoutes";
+import { configure } from "mobx";
+
+configure({
+    isolateGlobalState: true,
+});
 
 const initUI = () => {
     ReactDOM.render(<AppRoutes />, document.getElementById("root") as HTMLElement);
