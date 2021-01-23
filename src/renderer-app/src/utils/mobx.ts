6fae57a8dfe7b5abc17be36996880911489e65d1
crimx <straybugs@gmail.com>
2021-01-24 01:49:52 +0800

    feat: add mobx hooks

---
+++ src/renderer-app/src/utils/mobx.ts
@@ -0,0 +1,51 @@
+import {
+    autorun,
+    IAutorunOptions,
+    IReactionOptions,
+    IReactionPublic,
+    IWhenOptions,
+    reaction,
+    when,
+} from "mobx";
+import { useEffect, DependencyList } from "react";
+
+/**
+ * @param extraDeps provide extra dependencies to re-setup autorun if non-observable values are used
+ */
+export function useAutoRun(
+    view: (reaction: IReactionPublic) => void,
+    extraDeps: DependencyList = [],
+    opts?: IAutorunOptions,
+): void {
+    // MobX takes care of the deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => autorun(view, opts), extraDeps);
+}
+
+/**
+ * @param extraDeps provide extra dependencies to re-setup autorun if non-observable values are used
+ */
+export function useReaction<T>(
+    expression: (reaction: IReactionPublic) => T,
+    effect: (value: T, previousValue: T, reaction: IReactionPublic) => void,
+    extraDeps: DependencyList = [],
+    opts?: IReactionOptions,
+): void {
+    // MobX takes care of the deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => reaction(expression, effect, opts), extraDeps);
+}
+
+/**
+ * @param extraDeps provide extra dependencies to re-setup autorun if non-observable values are used
+ */
+export function useWhen(
+    predicate: () => boolean,
+    effect: () => void,
+    extraDeps: DependencyList = [],
+    otps?: IWhenOptions,
+): void {
+    // MobX takes care of the deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => when(predicate, effect, otps), extraDeps);
+}
