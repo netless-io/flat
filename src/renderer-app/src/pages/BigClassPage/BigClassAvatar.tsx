256db9331b7bd59463a0966ad1a954c298500d07
crimx <straybugs@gmail.com>
2021-01-25 11:20:19 +0800

    refactor: make video avatar observer

--- src/renderer-app/src/pages/BigClassPage/BigClassAvatar.tsx
+++ src/renderer-app/src/pages/BigClassPage/BigClassAvatar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
 import classNames from "classnames";
 import { VideoAvatar, VideoAvatarProps } from "../../components/VideoAvatar";
 
@@ -13,43 +14,42 @@ export interface BigClassAvatarProps extends Omit<VideoAvatarProps, "children"> 
     onExpand?: () => void;
 }
 
-export class BigClassAvatar extends React.PureComponent<BigClassAvatarProps> {
-    render(): React.ReactNode {
-        return <VideoAvatar {...this.props}>{this.renderMainContent}</VideoAvatar>;
-    }
-
-    private renderMainContent = (
-        canvas: React.ReactNode,
-        ctrlBtns: React.ReactNode,
-    ): React.ReactNode => {
-        const { avatarUser, small, onExpand } = this.props;
-        return (
-            <section className={classNames("big-class-avatar-wrap", { "is-small": small })}>
-                {canvas}
-                {!avatarUser.camera && (
-                    <div className="big-class-avatar-background">
-                        <img src={small ? noCameraSmall : noCamera} alt="no camera" />
-                    </div>
-                )}
-                <div
-                    className={classNames("big-class-avatar-ctrl-layer", {
-                        "with-video": avatarUser.camera,
-                    })}
-                >
-                    {small ? (
-                        <button className="big-class-avatar-expand" onClick={onExpand}>
-                            <img src={videoExpand} alt="expand" />
-                        </button>
-                    ) : (
-                        <>
-                            <h1 className="big-class-avatar-title">{avatarUser.name}</h1>
-                            {ctrlBtns}
-                        </>
+export const BigClassAvatar = observer<BigClassAvatarProps>(function BigClassAvatar({
+    avatarUser,
+    small,
+    onExpand,
+    ...restProps
+}) {
+    return (
+        <VideoAvatar {...restProps} avatarUser={avatarUser}>
+            {(canvas, ctrlBtns) => (
+                <section className={classNames("big-class-avatar-wrap", { "is-small": small })}>
+                    {canvas}
+                    {!avatarUser.camera && (
+                        <div className="big-class-avatar-background">
+                            <img src={small ? noCameraSmall : noCamera} alt="no camera" />
+                        </div>
                     )}
-                </div>
-            </section>
-        );
-    };
-}
+                    <div
+                        className={classNames("big-class-avatar-ctrl-layer", {
+                            "with-video": avatarUser.camera,
+                        })}
+                    >
+                        {small ? (
+                            <button className="big-class-avatar-expand" onClick={onExpand}>
+                                <img src={videoExpand} alt="expand" />
+                            </button>
+                        ) : (
+                            <>
+                                <h1 className="big-class-avatar-title">{avatarUser.name}</h1>
+                                {ctrlBtns}
+                            </>
+                        )}
+                    </div>
+                </section>
+            )}
+        </VideoAvatar>
+    );
+});
 
 export default BigClassAvatar;
