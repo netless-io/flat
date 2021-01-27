3bed20f3127a719b99ef2ba25c20bbea1acfa4da
crimx <straybugs@gmail.com>
2021-01-27 17:26:39 +0800

    feat: add one to one template

---
+++ src/renderer-app/src/pages/OneToOnePage/index.tsx
@@ -0,0 +1,11 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+
+export interface OneToOnePageProps {}
+
+export const OneToOnePage = observer(function OneToOnePage() {
+    // @TODO
+    return <></>;
+});
+
+export default OneToOnePage;
