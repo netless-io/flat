80c0660709b09c901f688ccd81c99a06d4daad32
Black-Hole1 <158blackhole@gmail.com>
2021-01-27 17:06:53 +0800

    feat(device-detect): add microphone detect

--- src/renderer-app/src/pages/UserSettingPanel/MicrophoneTesting.tsx
+++ src/renderer-app/src/pages/UserSettingPanel/MicrophoneTesting.tsx
@@ -1,22 +1,75 @@
-import React from "react";
-import { Select, Slider, Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button } from "antd";
 import { Link } from "react-router-dom";
 import "./MicrophoneTesting.less";
+import type AgoraSDK from "agora-electron-sdk";
+import { AGORA } from "../../constants/Process";
+import { DeviceSelect } from "../../components/DeviceSelect";
+import { Device } from "../../types/Device";
+import { observer } from "mobx-react-lite";
 
 interface SpeakerVolumeProps {
     percent: number;
 }
 
-const SpeakerVolume = ({ percent }: SpeakerVolumeProps): React.ReactElement => {
+const SpeakerVolume = observer<SpeakerVolumeProps>(function SpeakerVolume({ percent }) {
     return (
         <div className="speaker-audio-wrapper">
-            <div className="speaker-audio-volume" style={{ width: `${percent}%` }}></div>
-            <div className="speaker-audio-mask"></div>
+            <div className="speaker-audio-volume" style={{ width: `${percent}%` }} />
+            <div className="speaker-audio-mask" />
         </div>
     );
-};
+});
 
 export const MicrophoneTesting = (): React.ReactElement => {
+    const [rtcEngine] = useState<AgoraSDK>(() => new window.AgoraRtcEngine());
+    const [devices, setDevices] = useState<Device[]>([]);
+    const [currentDeviceID, setCurrentDeviceID] = useState<string | null>(null);
+    const [currentVolume, setCurrentVolume] = useState(0);
+
+    useEffect(() => {
+        rtcEngine.initialize(AGORA.APP_ID);
+
+        setDevices(rtcEngine.getAudioRecordingDevices() as Device[]);
+
+        rtcEngine.on("audioDeviceStateChanged", () => {
+            setDevices(rtcEngine.getVideoDevices() as Device[]);
+        });
+
+        return () => {
+            rtcEngine.removeAllListeners();
+            rtcEngine.release();
+        };
+    }, [rtcEngine]);
+
+    useEffect(() => {
+        if (devices.length !== 0) {
+            setCurrentDeviceID(devices[0].deviceid);
+        }
+    }, [devices]);
+
+    useEffect(() => {
+        const groupAudioVolumeIndication = (
+            _speakers: any,
+            _speakerNumber: any,
+            totalVolume: number,
+        ): void => {
+            // totalVolume value max 255
+            setCurrentVolume(Math.ceil((totalVolume / 255) * 100));
+        };
+
+        if (currentDeviceID) {
+            rtcEngine.setAudioRecordingDevice(currentDeviceID);
+            rtcEngine.on("groupAudioVolumeIndication", groupAudioVolumeIndication);
+            rtcEngine.startAudioRecordingDeviceTest(300);
+        }
+
+        return () => {
+            rtcEngine.removeListener("groupAudioVolumeIndication", groupAudioVolumeIndication);
+            rtcEngine.stopAudioRecordingDeviceTest();
+        };
+    }, [currentDeviceID, rtcEngine]);
+
     return (
         <div className="content-container">
             <div className="header-container">
@@ -24,16 +77,13 @@ export const MicrophoneTesting = (): React.ReactElement => {
             </div>
             <div className="speaker-testing-container">
                 <p>麦克风</p>
-                <Select defaultValue="MacBook Pro 麦克风">
-                    <Select.Option value="cheerchen">MacBook Pro 麦克风</Select.Option>
-                </Select>
-                {/* TODO  MicrophoneTesting*/}
+                <DeviceSelect
+                    devices={devices}
+                    currentDeviceID={currentDeviceID}
+                    onChange={setCurrentDeviceID}
+                />
                 <p>试听声音</p>
-                <SpeakerVolume percent={30} />
-                <div className="speaker-audio-testing">
-                    <p>音量</p>
-                    <Slider defaultValue={30} />
-                </div>
+                <SpeakerVolume percent={currentVolume} />
                 <div className="testing-btn">
                     <Button>
                         <Link to="/setting/microphone/">不可以听到</Link>
