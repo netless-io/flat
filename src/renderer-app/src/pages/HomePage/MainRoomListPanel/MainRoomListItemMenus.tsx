1ea581f0b097c405215ec78a83680d1bb5a79e2b
hyrious <hyrious@outlook.com>
2021-01-29 15:36:13 +0800

    feat: cancel room modal (todo: ScheduleRoomDetailPage)

--- src/renderer-app/src/pages/HomePage/MainRoomListPanel/MainRoomListItemMenus.tsx
+++ src/renderer-app/src/pages/HomePage/MainRoomListPanel/MainRoomListItemMenus.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Menu } from "antd";
+import { Button, Checkbox, Menu, message, Modal } from "antd";
 import { MenuProps } from "antd/lib/menu";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { cancelHistoryRoom } from "../../../apiMiddleware/flatServer";
+import { RoomStoreContext } from "../../../components/StoreProvider";
 import { generateRoutePath, RouteNameType } from "../../../utils/routes";
-import {
-    cancelHistoryRoom,
-    cancelOrdinaryRoom,
-    cancelPeriodicSubRoom,
-} from "../../../apiMiddleware/flatServer";
 
 export interface MainRoomListItemMenusProps extends MenuProps {
     roomUUID: string;
@@ -23,54 +20,100 @@ export const MainRoomListItemMenus = React.memo<MainRoomListItemMenusProps>(
         onClick,
         ...restProps
     }) {
+        const [cancelModalVisible, setCancelModalVisible] = useState(false);
+        const [isCancelAll, setIsCancelAll] = useState(false);
+        const roomStore = useContext(RoomStoreContext);
+
         return (
             // pass down props so that antd dropdrown menu shadow is rendered properly
-            <Menu {...restProps} onClick={e => e.domEvent.stopPropagation()}>
-                <Menu.Item>
-                    <Link
-                        to={{
-                            pathname: generateRoutePath(RouteNameType.RoomDetailPage, {
-                                roomUUID,
-                                periodicUUID,
-                            }),
-                        }}
-                    >
-                        房间详情
-                    </Link>
-                </Menu.Item>
-                {isHistoryList ? (
-                    <Menu.Item onClick={deleteRoomHistory}>删除记录</Menu.Item>
-                ) : (
-                    <>
-                        <Menu.Item>
-                            <Link
-                                to={{
-                                    pathname: generateRoutePath(
-                                        RouteNameType.ModifyOrdinaryRoomPage,
-                                        {
-                                            roomUUID,
-                                            periodicUUID: periodicUUID || void 0,
-                                        },
-                                    ),
-                                }}
-                            >
-                                修改房间
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item onClick={cancelRoom}>取消房间</Menu.Item>
-                        <Menu.Item>复制邀请</Menu.Item>
-                    </>
-                )}
-            </Menu>
+            <>
+                <Menu {...restProps} onClick={e => e.domEvent.stopPropagation()}>
+                    <Menu.Item>
+                        <Link
+                            to={{
+                                pathname: generateRoutePath(RouteNameType.RoomDetailPage, {
+                                    roomUUID,
+                                    periodicUUID,
+                                }),
+                            }}
+                        >
+                            房间详情
+                        </Link>
+                    </Menu.Item>
+                    {isHistoryList ? (
+                        <Menu.Item onClick={deleteRoomHistory}>删除记录</Menu.Item>
+                    ) : (
+                        <>
+                            <Menu.Item>
+                                <Link
+                                    to={{
+                                        pathname: generateRoutePath(
+                                            RouteNameType.ModifyOrdinaryRoomPage,
+                                            {
+                                                roomUUID,
+                                                periodicUUID: periodicUUID || void 0,
+                                            },
+                                        ),
+                                    }}
+                                >
+                                    修改房间
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item onClick={showCancelRoomModal}>取消房间</Menu.Item>
+                            <Menu.Item>复制邀请</Menu.Item>
+                        </>
+                    )}
+                </Menu>
+                <Modal
+                    visible={cancelModalVisible}
+                    title="取消房间"
+                    onCancel={hideCancelModal}
+                    onOk={confirmCancelRoom}
+                    footer={[
+                        <Button key="Cancel" onClick={hideCancelModal}>
+                            再想想
+                        </Button>,
+                        <Button key="Ok" type="primary" onClick={confirmCancelRoom}>
+                            确定
+                        </Button>,
+                    ]}
+                >
+                    {periodicUUID ? (
+                        <Checkbox
+                            checked={isCancelAll}
+                            onChange={e => setIsCancelAll(e.target.checked)}
+                        >
+                            取消该系列全部周期性房间
+                        </Checkbox>
+                    ) : (
+                        "确定取消该房间吗？"
+                    )}
+                </Modal>
+            </>
         );
+
+        function hideCancelModal(): void {
+            setCancelModalVisible(false);
+        }
+
+        async function confirmCancelRoom(): Promise<void> {
+            setCancelModalVisible(false);
+            await cancelRoom();
+        }
+
+        function showCancelRoomModal(): void {
+            setIsCancelAll(false);
+            setCancelModalVisible(true);
+        }
 
         async function cancelRoom(): Promise<void> {
             try {
-                if (periodicUUID) {
-                    await cancelPeriodicSubRoom({ roomUUID, periodicUUID });
-                } else {
-                    await cancelOrdinaryRoom(roomUUID);
-                }
+                await roomStore.cancelRoom({
+                    all: isCancelAll,
+                    roomUUID,
+                    periodicUUID,
+                });
+                message.success("已取消该房间");
             } catch (e) {
                 console.error(e);
             }
