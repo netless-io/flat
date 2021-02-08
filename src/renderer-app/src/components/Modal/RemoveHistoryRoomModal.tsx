1a50d353db69042df02e03a8e8f86911282f642c
Black-Hole1 <158blackhole@gmail.com>
2021-02-08 15:20:27 +0800

    feat(modal): add remove history room modal

---
+++ src/renderer-app/src/components/Modal/RemoveHistoryRoomModal.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { observer } from "mobx-react-lite";
+import { Button, Modal } from "antd";
+
+interface RemoveHistoryRoomModalProps {
+    visible: boolean;
+    onCancel: () => void;
+    onConfirm: () => void;
+    loading: boolean;
+}
+
+export const RemoveHistoryRoomModal = observer<RemoveHistoryRoomModalProps>(
+    function RemoveHistoryRoomModal({ visible, onCancel, onConfirm, loading }) {
+        return (
+            <Modal
+                title={"删除房间记录"}
+                visible={visible}
+                onCancel={onCancel}
+                footer={[
+                    <Button key="exit-cancel" onClick={onCancel}>
+                        取消
+                    </Button>,
+                    <Button key="exit-confirm" danger loading={loading} onClick={onConfirm}>
+                        确认
+                    </Button>,
+                ]}
+            >
+                确认删除当前房间记录？
+            </Modal>
+        );
+    },
+);
