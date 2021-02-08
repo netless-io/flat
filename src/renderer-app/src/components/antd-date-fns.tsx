2128e9088e329765a35cbca002f7dd2b632fe7cb
crimx <straybugs@gmail.com>
2021-02-08 11:43:35 +0800

    fix: pass getPopupContainer from context to DatePicker

--- src/renderer-app/src/components/antd-date-fns.tsx
+++ src/renderer-app/src/components/antd-date-fns.tsx
@@ -1,16 +1,35 @@
 // https://ant.design/docs/react/replace-moment
 
-import React from "react";
+import React, { FC, useContext } from "react";
 import dateFnsGenerateConfig from "rc-picker/lib/generate/dateFns";
-import generatePicker, { PickerTimeProps } from "antd/es/date-picker/generatePicker";
+import generatePicker, { PickerTimeProps, PickerProps } from "antd/es/date-picker/generatePicker";
+import { ConfigContext } from "antd/lib/config-provider";
 import "antd/es/date-picker/style/index";
 
-export const DatePicker = generatePicker<Date>(dateFnsGenerateConfig);
+export type DatePickerProps = PickerProps<Date>;
 
-export interface TimePickerProps extends Omit<PickerTimeProps<Date>, "picker"> {}
+const DatePickerInner = generatePicker<Date>(dateFnsGenerateConfig);
 
-export const TimePicker = React.forwardRef<any, TimePickerProps>((props, ref) => {
-    return <DatePicker {...props} picker="time" mode={undefined} ref={ref} />;
-});
+export const DatePicker: FC<DatePickerProps> = props => {
+    // For some reason DatePickerInner does not receive configs from ConfigContext.
+    // Pass them to DatePickerInner manually.
+    const { getPopupContainer: getContextPopupContainer, locale: localeContext } = useContext(
+        ConfigContext,
+    );
+
+    return (
+        <DatePickerInner
+            {...props}
+            locale={props.locale || localeContext?.DatePicker}
+            getPopupContainer={props.getPopupContainer || getContextPopupContainer}
+        />
+    );
+};
+
+export type TimePickerProps = Omit<PickerTimeProps<Date>, "picker">;
+
+export const TimePicker: FC<TimePickerProps> = props => {
+    return <DatePicker {...props} picker="time" mode={undefined} />;
+};
 
 TimePicker.displayName = "TimePicker";
