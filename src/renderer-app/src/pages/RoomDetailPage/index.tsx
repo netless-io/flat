5ac8d9f85975bb97dd32b44170e005565860311f
hyrious <hyrious@outlook.com>
2021-02-09 18:14:45 +0800

    chore: inverse history rooms

--- src/renderer-app/src/pages/RoomDetailPage/index.tsx
+++ src/renderer-app/src/pages/RoomDetailPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { format } from "date-fns";
 import { zhCN } from "date-fns/locale";
 import { Link, useParams } from "react-router-dom";
