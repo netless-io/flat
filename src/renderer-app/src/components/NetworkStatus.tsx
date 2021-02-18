9701a6bd9d490489ae56c7b608d483b7892538f6
crimx <straybugs@gmail.com>
2021-02-18 18:18:21 +0800

    feat(classroom): add network quality check

--- src/renderer-app/src/components/NetworkStatus.tsx
+++ src/renderer-app/src/components/NetworkStatus.tsx
@@ -1,27 +1,72 @@
-import React from "react";
+import signal0 from "../assets/image/signal-0.svg";
 import signal1 from "../assets/image/signal-1.svg";
 import signal2 from "../assets/image/signal-2.svg";
 import signal3 from "../assets/image/signal-3.svg";
 import "./NetworkStatus.less";
 
+import React from "react";
+import { observer } from "mobx-react-lite";
+import { useComputed } from "../utils/mobx";
+
+const signalLocale = [
+    "质量未知",
+    "网络质量极好",
+    "网络质量优秀",
+    "网络质量一般",
+    "网络质量较差",
+    "网络质量非常差",
+    "网络连接已断开",
+    "暂时无法检测网络质量",
+    "正在检测...",
+];
+
 export interface NetworkStatusProps {
-    // @TODO implement logic
+    networkQuality: {
+        delay: number;
+        uplink: number;
+        downlink: number;
+    };
 }
 
-export class NetworkStatus extends React.PureComponent<NetworkStatusProps> {
-    private signal = [signal1, signal2, signal3];
+export const NetworkStatus = observer<NetworkStatusProps>(function NetworkStatus({
+    networkQuality,
+}) {
+    const signalIcon = useComputed<string>(() => {
+        const { uplink, downlink } = networkQuality;
+        if (uplink === 5 || downlink === 5 || uplink === 4 || downlink === 4) {
+            return signal1;
+        }
+        if (uplink === 3 || downlink === 3) {
+            return signal2;
+        }
+        if (uplink === 2 || downlink === 2 || uplink === 1 || downlink === 1) {
+            return signal3;
+        }
+        if (uplink === 8 || downlink === 8) {
+            // checking
+            return signal3;
+        }
+        return signal0;
+    }).get();
 
-    render(): React.ReactNode {
-        return (
-            <div className="network-status">
-                <span className="network-status-delay">延迟：0ms</span>
-                <span className="network-status-signal">
-                    网络：
-                    <img src={this.signal[2]} alt="signal" />
-                </span>
-            </div>
-        );
-    }
-}
+    const signalText = useComputed<string>(
+        () =>
+            `上行：${signalLocale[networkQuality.uplink]}，下行：${
+                signalLocale[networkQuality.downlink]
+            }`,
+    ).get();
+
+    return (
+        <div className="network-status">
+            <span className="network-status-delay" title="客户端到边缘服务器的网络延迟">
+                延迟：{networkQuality.delay}ms
+            </span>
+            <span className="network-status-signal" title={signalText}>
+                网络：
+                <img src={signalIcon} alt={signalText} />
+            </span>
+        </div>
+    );
+});
 
 export default NetworkStatus;
