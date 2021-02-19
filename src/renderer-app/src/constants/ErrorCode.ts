86d7fe0e8e13d8b0bb77d503d764a8bf7683a10e
Black-Hole1 <158blackhole@gmail.com>
2021-02-19 14:54:27 +0800

    chore(error-code): improve error message

--- src/renderer-app/src/constants/ErrorCode.ts
+++ src/renderer-app/src/constants/ErrorCode.ts
@@ -29,14 +29,14 @@ export const RequestErrorMessage = {
     [RequestErrorCode.ServerFail]: "请求失败",
     [RequestErrorCode.CurrentProcessFailed]: "请求出错",
     [RequestErrorCode.NotPermission]: "没有权限操作",
-    [RequestErrorCode.NeedLoginAgain]: "请先登陆",
-    [RequestErrorCode.UnsupportedPlatform]: "不支持的登陆平台",
-    [RequestErrorCode.JWTSignFailed]: "认证信息校验失败",
+    [RequestErrorCode.NeedLoginAgain]: "凭证已过期，请重新登录",
+    [RequestErrorCode.UnsupportedPlatform]: "不支持的登录平台",
+    [RequestErrorCode.JWTSignFailed]: "认证信息校验失败，请重新登录",
 
     [RequestErrorCode.RoomNotFound]: "房间不存在",
     [RequestErrorCode.RoomIsEnded]: "房间已结束",
     [RequestErrorCode.RoomIsRunning]: "房间正在进行中",
-    [RequestErrorCode.RoomNotIsRunning]: "房间不在运行中",
+    [RequestErrorCode.RoomNotIsRunning]: "房间不在进行中",
     [RequestErrorCode.RoomNotIsEnded]: "房间还未结束",
     [RequestErrorCode.RoomNotIsIdle]: "房间还未开始",
 
