f9aaf42aae2d702fcf03e701d5d5ab7231a9bfc4
CRIMX <straybugs@gmail.com>
2021-02-23 16:19:42 +0800

    refactor(app-routes): add app route container (#320)
    * refactor(app-routes): add app route container
    
    * refactor(app-routes): clear selection on switching routes

--- src/renderer-app/src/PageError.tsx
+++ src/renderer-app/src/PageError.tsx
@@ -1,30 +1,45 @@
-import React from "react";
-import "./PageError.less";
 import room_not_find from "./assets/image/room_not_find.svg";
-import {Button} from "antd";
-import {Link} from "react-router-dom";
+import "./PageError.less";
 
-export default class PageError extends React.Component<{}, {}> {
-    public render(): React.ReactNode {
-        return (
-            <div className="page404-box">
-                <div className="page404-image-box">
-                    <img className="page404-image-inner" src={room_not_find}/>
-                    <div className="page404-inner">
-                        <div className="page404-inner-title">
-                            抱歉，您要访问的页面不存在
-                        </div>
-                        <div className="page404-inner-script">
-                            可能是链接地址有误，页面已经被移除或者隐藏
-                        </div>
-                        <Link to={"/"}>
-                            <Button size={"large"} style={{width: 118}}>
-                                返回首页
-                            </Button>
-                        </Link>
-                    </div>
+import React, { useEffect, useState } from "react";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { RouteNameType, usePushHistory } from "./utils/routes";
+
+export const PageError = React.memo(function PageError() {
+    const [countDown, setCountDown] = useState(15);
+    const pushHistory = usePushHistory();
+
+    useEffect(() => {
+        let countDown = 15;
+        const ticket = window.setInterval(() => {
+            if (--countDown < 0) {
+                window.clearInterval(ticket);
+                pushHistory(RouteNameType.HomePage);
+                return;
+            }
+            setCountDown(countDown);
+        }, 1000);
+
+        return () => {
+            window.clearInterval(ticket);
+        };
+    }, [pushHistory]);
+
+    return (
+        <div className="page404-box">
+            <div className="page404-image-box">
+                <img className="page404-image-inner" src={room_not_find} />
+                <div className="page404-inner">
+                    <div className="page404-inner-title">抱歉，遇到未知错误</div>
+                    <div className="page404-inner-script">请重试</div>
+                    <Link to={"/"}>
+                        <Button size="large">({countDown}) 返回首页</Button>
+                    </Link>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+});
+
+export default PageError;
