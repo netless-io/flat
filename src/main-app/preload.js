8b8993d12995d60db012b3c1df8adfa9d9f90793
Black-Hole <158blackhole@gmail.com>
2021-03-03 15:56:05 +0800

    fix(main): preload injection agora-electron-sdk probability of failure (#352)

--- src/main-app/preload.js
+++ src/main-app/preload.js
@@ -1,23 +1,21 @@
 const fixJQueryHosts = ["open.weixin.qq.com"];
 
-if (fixJQueryHosts.includes(location.host)) {
-    document.addEventListener("DOMNodeInserted", function fixJQuery() {
-        if (window && !window.$) {
-            window.$ = window.jQuery = require("jquery");
-            document.removeEventListener("DOMNodeInserted", fixJQuery);
-        }
-    });
-}
+document.addEventListener("DOMNodeInserted", function DOMNodeInserted() {
+    if (!window) {
+        return;
+    }
 
-if (
-    (location.protocol === "file:" && location.pathname.endsWith("static/render/index.html")) ||
-    location.hostname === "localhost" ||
-    location.hostname === "127.0.0.1"
-) {
-    document.addEventListener("DOMNodeInserted", function injectionAgoraAddon() {
-        if (window) {
-            window.AgoraRtcEngine = require("agora-electron-sdk").default;
-            document.removeEventListener("DOMNodeInserted", injectionAgoraAddon);
-        }
-    });
-}
+    if (fixJQueryHosts.includes(location.host) && !window.$) {
+        window.$ = window.jQuery = require("jquery");
+    }
+
+    if (
+        (location.protocol === "file:" && location.pathname.endsWith("static/render/index.html")) ||
+        location.hostname === "localhost" ||
+        location.hostname === "127.0.0.1"
+    ) {
+        window.AgoraRtcEngine = require("agora-electron-sdk").default;
+    }
+
+    document.removeEventListener("DOMNodeInserted", DOMNodeInserted);
+});
