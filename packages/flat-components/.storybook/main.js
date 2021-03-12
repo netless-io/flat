3d0ad5101a1b0a30d2b27edbc9e957b15f31bad9
crimx <straybugs@gmail.com>
2021-03-12 12:08:47 +0800

    refactor(storybook): remove js and jsx stories

--- packages/flat-components/.storybook/main.js
+++ packages/flat-components/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(ts|tsx)"],
     addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
