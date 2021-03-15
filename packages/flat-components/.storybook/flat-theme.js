6502433007f025b9e11ccae4f88b35cec4b5f6d8
CRIMX <straybugs@gmail.com>
2021-03-15 13:59:46 +0800

    feat(flat-components): add storybook flat brand image (#389)

---
+++ packages/flat-components/.storybook/flat-theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+import brandImage from "./flat-components.png";
+
+export default create({
+    base: "light",
+    brandTitle: "Flat Components",
+    brandUrl: "https://github.com/netless-io/flat",
+    brandImage: brandImage,
+});
