bdbaf544a652486e74aad3dd1c17ba27c523572d
crimx <straybugs@gmail.com>
2021-03-16 09:31:02 +0800

    fix(flat-component): fix type error

--- packages/flat-components/src/components/CloudStorage/index.tsx
+++ packages/flat-components/src/components/CloudStorage/index.tsx
@@ -2,10 +2,10 @@ import "./style.less";
 
 import React from "react";
 import { observer } from "mobx-react-lite";
-import { CloudStorageFileList } from "./CloudStorageFileList";
 
 export interface CloudStorageProps {}
 
 export const CloudStorage = observer<CloudStorageProps>(function CloudStorage() {
-    return <CloudStorageFileList files={[]} />;
+    // @TODO
+    return <></>;
 });
