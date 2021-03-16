920f540dccba7cd7a7ac5fbd025fcf615ddd6643
hyrious <hyrious@outlook.com>
2021-03-16 10:41:17 +0800

    chore: setup flat-web-react (#401)
    [ci ckip]

---
+++ web/react/src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return <div>Hello, world!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
