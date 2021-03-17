150e6261e16f04e5055c3f3571fea7d1f3307d8c
CRIMX <straybugs@gmail.com>
2021-03-17 22:08:45 +0800

    refactor(flat-components): inline images (#410)

--- packages/flat-components/.storybook/main.js
+++ packages/flat-components/.storybook/main.js
@@ -4,15 +4,40 @@ module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(ts|tsx)"],
     addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
     webpackFinal: async config => {
-        config.module.rules.unshift({
-            test: /\.less$/,
-            sideEffects: true,
-            use: [
-                { loader: require.resolve("style-loader") },
-                { loader: require.resolve("css-loader") },
-                { loader: require.resolve("less-loader") },
-            ],
+        config.module.rules = config.module.rules.filter(rule => {
+            return !(rule.loader && rule.loader.includes("file-loader"));
         });
+
+        config.module.rules.unshift(
+            {
+                test: /\.less$/,
+                sideEffects: true,
+                use: [
+                    { loader: require.resolve("style-loader") },
+                    { loader: require.resolve("css-loader") },
+                    { loader: require.resolve("less-loader") },
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|ico|apng)(\?.*)?$/i,
+                use: [
+                    {
+                        loader: require.resolve("url-loader"),
+                        options: {
+                            limit: 14336,
+                            name: "static/media/[name].[hash:8].[ext]",
+                            esModule: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/i,
+                loader: require.resolve("file-loader"),
+                options: { name: "static/media/[name].[hash:8].[ext]", esModule: false },
+            },
+        );
+
         return config;
     },
 };
