82ce25abc64a29505c2d7c8303d703a37895dbc0
Black-Hole <158blackhole@gmail.com>
2021-03-19 15:36:59 +0800

    refactor(main-app): remove useless menu (#422)
    1. remove all menu in Windows system
    2. only keep about / hide / hideOther / quit

--- desktop/main-app/src/bootup/Init-menus.ts
+++ desktop/main-app/src/bootup/Init-menus.ts
@@ -2,95 +2,30 @@ import { app, Menu, MenuItemConstructorOptions } from "electron";
 import runtime from "../utils/Runtime";
 
 export default () => {
+    if (runtime.isWin) {
+        Menu.setApplicationMenu(Menu.buildFromTemplate([]));
+        return;
+    }
+
     const appByMacMenu: IMenu = {
         label: app.name,
         submenu: [
-            { role: "about" },
-            { type: "separator" },
-            { role: "services" },
-            { type: "separator" },
-            { role: "hide" },
-            { role: "hideOthers" },
-            { role: "unhide" },
-            { type: "separator" },
-            { role: "quit" },
-        ],
-    };
-
-    const fileMenu: IMenu = {
-        label: "File",
-        submenu: [{ role: "close" }],
-    };
-    if (runtime.isWin) {
-        fileMenu.submenu = [{ role: "quit" }];
-    }
-
-    const editMenu: IMenu = {
-        label: "Edit",
-        submenu: [
-            { role: "undo" },
-            { role: "redo" },
-            { type: "separator" },
-            { role: "cut" },
-            { role: "copy" },
-            { role: "paste" },
-        ],
-    };
-    if (runtime.isWin) {
-        editMenu.submenu.push({ role: "delete" }, { type: "separator" }, { role: "selectAll" });
-    } else {
-        editMenu.submenu.push(
-            { role: "pasteAndMatchStyle" },
-            { role: "delete" },
-            { role: "selectAll" },
-            { type: "separator" },
             {
-                label: "Speech",
-                submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+                role: "about",
             },
-        );
-    }
-
-    const windowMenu: IMenu = {
-        label: "Window",
-        submenu: [{ role: "minimize" }, { role: "zoom" }],
+            {
+                role: "hide",
+            },
+            {
+                role: "hideOthers",
+            },
+            {
+                role: "quit",
+            },
+        ],
     };
-    if (runtime.isWin) {
-        windowMenu.submenu.push({ role: "close" });
-    } else {
-        windowMenu.submenu.push(
-            { type: "separator" },
-            { role: "front" },
-            { type: "separator" },
-            { role: "window" },
-        );
-    }
 
-    const template: MenuItemConstructorOptions[] = [
-        editMenu,
-        fileMenu,
-        {
-            label: "View",
-            submenu: [
-                { role: "reload" },
-                { role: "forceReload" },
-                { role: "toggleDevTools" },
-                { type: "separator" },
-                { role: "resetZoom" },
-                { role: "zoomIn" },
-                { role: "zoomOut" },
-                { type: "separator" },
-                { role: "togglefullscreen" },
-            ],
-        },
-        windowMenu,
-    ];
-
-    if (runtime.isMac) {
-        template.unshift(appByMacMenu);
-    }
-
-    const menu = Menu.buildFromTemplate(template);
+    const menu = Menu.buildFromTemplate([appByMacMenu]);
     Menu.setApplicationMenu(menu);
 };
 
