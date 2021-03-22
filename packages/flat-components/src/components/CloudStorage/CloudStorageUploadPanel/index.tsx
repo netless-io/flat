3c61baa8fb93540697c1155c7dd4c07f9507e083
CRIMX <straybugs@gmail.com>
2021-03-22 10:45:17 +0800

    feat(flat-components): add upload panel (#428)

---
+++ packages/flat-components/src/components/CloudStorage/CloudStorageUploadPanel/index.tsx
@@ -0,0 +1,77 @@
+import "./style.less";
+
+import { ExclamationCircleOutlined, UpOutlined, CloseOutlined } from "@ant-design/icons";
+import React, { FC, useState } from "react";
+import { Button } from "antd";
+import classNames from "classnames";
+import { ResizeReporter } from "react-resize-reporter/scroll";
+
+export interface CloudStorageUploadPanelProps
+    extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+    /** Max Panel Height */
+    maxHeight?: number;
+    /** If at least one failed upload */
+    hasError?: boolean;
+    /** Should expand panel */
+    expand: boolean;
+    /** Number of finished upload */
+    finished: number;
+    /** Number of total upload */
+    total: number;
+    /** Panel expand button clicked */
+    onExpandChange: (isExpand: boolean) => void;
+    /** Panel close button clicked */
+    onClose: (event?: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const CloudStorageUploadPanel: FC<CloudStorageUploadPanelProps> = ({
+    maxHeight = 260,
+    hasError,
+    expand,
+    finished,
+    total,
+    className,
+    children,
+    onExpandChange,
+    onClose,
+    ...restProps
+}) => {
+    const [contentHeight, setContentHeight] = useState(0);
+
+    return (
+        <section {...restProps} className={classNames(classNames, "cloud-storage-upload-panel")}>
+            <header className="cloud-storage-upload-panel-head">
+                {hasError ? (
+                    <>
+                        <ExclamationCircleOutlined className="cloud-storage-upload-panel-warning" />{" "}
+                        <h1 className="cloud-storage-upload-panel-title">上传异常</h1>
+                    </>
+                ) : (
+                    <h1 className="cloud-storage-upload-panel-title">传输列表</h1>
+                )}
+                <div className="cloud-storage-upload-panel-count">
+                    {finished}/{total}
+                </div>
+                <div className="cloud-storage-upload-panel-head-btns">
+                    <Button shape="circle" size="small" onClick={() => onExpandChange(!expand)}>
+                        <UpOutlined rotate={expand ? 180 : 0} />
+                    </Button>
+                    <Button shape="circle" size="small" onClick={onClose}>
+                        <CloseOutlined />
+                    </Button>
+                </div>
+            </header>
+            <div
+                className="cloud-storage-upload-panel-content"
+                style={{
+                    height: expand ? (contentHeight < maxHeight ? contentHeight : maxHeight) : 0,
+                }}
+            >
+                <div className="cloud-storage-upload-panel-content-sizer">
+                    <ResizeReporter reportInit onHeightChanged={setContentHeight} />
+                    <div className="cloud-storage-upload-panel-status-list">{children}</div>
+                </div>
+            </div>
+        </section>
+    );
+};
