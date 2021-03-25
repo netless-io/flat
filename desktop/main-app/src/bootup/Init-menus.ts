43a862d9dc9286638c966f28ddd9f4bdc090d3ba
hyrious <hyrious@outlook.com>
2021-03-25 15:58:13 +0800

    refactor(window): revert menus, auto hide it on windows (#449)

--- desktop/main-app/src/bootup/Init-menus.ts
+++ desktop/main-app/src/bootup/Init-menus.ts
@@ -2,30 +2,95 @@ import { app, Menu, MenuItemConstructorOptions } from "electron";
 import runtime from "../utils/Runtime";
 
 export default () => {
-    if (runtime.isWin) {
-        Menu.setApplicationMenu(Menu.buildFromTemplate([]));
-        return;
-    }
-
     const appByMacMenu: IMenu = {
         label: app.name,
         submenu: [
-            {
-                role: "about",
-            },
-            {
-                role: "hide",
-            },
-            {
-                role: "hideOthers",
-            },
-            {
-                role: "quit",
-            },
+            { role: "about" },
+            { type: "separator" },
+            { role: "services" },
+            { type: "separator" },
+            { role: "hide" },
+            { role: "hideOthers" },
+            { role: "unhide" },
+            { type: "separator" },
+            { role: "quit" },
         ],
     };
 
-    const menu = Menu.buildFromTemplate([appByMacMenu]);
+    const fileMenu: IMenu = {
+        label: "File",
+        submenu: [{ role: "close" }],
+    };
+    if (runtime.isWin) {
+        fileMenu.submenu = [{ role: "quit" }];
+    }
+
+    const editMenu: IMenu = {
+        label: "Edit",
+        submenu: [
+            { role: "undo" },
+            { role: "redo" },
+            { type: "separator" },
+            { role: "cut" },
+            { role: "copy" },
+            { role: "paste" },
+        ],
+    };
+    if (runtime.isWin) {
+        editMenu.submenu.push({ role: "delete" }, { type: "separator" }, { role: "selectAll" });
+    } else {
+        editMenu.submenu.push(
+            { role: "pasteAndMatchStyle" },
+            { role: "delete" },
+            { role: "selectAll" },
+            { type: "separator" },
+            {
+                label: "Speech",
+                submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+            },
+        );
+    }
+
+    const windowMenu: IMenu = {
+        label: "Window",
+        submenu: [{ role: "minimize" }, { role: "zoom" }],
+    };
+    if (runtime.isWin) {
+        windowMenu.submenu.push({ role: "close" });
+    } else {
+        windowMenu.submenu.push(
+            { type: "separator" },
+            { role: "front" },
+            { type: "separator" },
+            { role: "window" },
+        );
+    }
+
+    const template: MenuItemConstructorOptions[] = [
+        editMenu,
+        fileMenu,
+        {
+            label: "View",
+            submenu: [
+                { role: "reload" },
+                { role: "forceReload" },
+                { role: "toggleDevTools" },
+                { type: "separator" },
+                { role: "resetZoom" },
+                { role: "zoomIn" },
+                { role: "zoomOut" },
+                { type: "separator" },
+                { role: "togglefullscreen" },
+            ],
+        },
+        windowMenu,
+    ];
+
+    if (runtime.isMac) {
+        template.unshift(appByMacMenu);
+    }
+
+    const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
 };
 
--- desktop/main-app/src/utils/WindowManager.ts
+++ desktop/main-app/src/utils/WindowManager.ts
@@ -63,6 +63,8 @@ export class WindowManager {
 
         windowReadyToShow(innerWin);
 
+        innerWin.window.setAutoHideMenuBar(true);
+
         return innerWin;
     }
 
