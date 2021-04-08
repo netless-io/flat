9b81c8c137c34aa2c9b980e9405a26e2769c435f
hyrious <hyrious@outlook.com>
2021-04-08 14:23:27 +0800

    feat(render): add preloader utils (#502)

--- desktop/main-app/src/bootup/Init-webRequest.ts
+++ desktop/main-app/src/bootup/Init-webRequest.ts
@@ -1,9 +1,16 @@
-import { session } from "electron";
+import { protocol, session } from "electron";
 import fs from "fs-extra";
 import path from "path";
 import runtime from "../utils/Runtime";
 
 export default () => {
+    // electron 9 版本之后，下面的代码必须加上，否则因为 electron 无法处理 file 协议而导致 预加载失效
+    // 详情可见: https://github.com/electron/electron/issues/23757
+    protocol.registerFileProtocol("file", (request, callback) => {
+        const pathname = decodeURI(request.url.replace("file:///", ""));
+        callback(pathname);
+    });
+
     const filter = {
         urls: ["https://convertcdn.netless.link/*"],
     };
