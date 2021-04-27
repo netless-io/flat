e54df44746a2ddeddff7105868d38e3e70d6e0e5
Black-Hole1 <158blackhole@gmail.com>
2021-04-27 16:27:20 +0800

    fix(app): application flickers
    1. disable hardware acceleration

--- desktop/main-app/src/bootup/Init-env.ts
+++ desktop/main-app/src/bootup/Init-env.ts
@@ -1,7 +1,11 @@
+import { app } from "electron";
+
 export default () => {
     // disable warning: "kq_init: detected broken kqueue; not using."
     // link: https://github.com/tmux/tmux/issues/475
     // but i don’t know where tmux is referenced. If you can find it and submit a PR, we would be very grateful.
     // @ts-ignore
     process.env.EVENT_NOKQUEUE = 1;
+
+    app.disableHardwareAcceleration();
 };
