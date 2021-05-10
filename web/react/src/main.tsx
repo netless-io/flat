0fd0f08461941f91f9ec4f9bc010de73a3206111
hyrious <hyrious@outlook.com>
2021-05-10 11:36:04 +0800

    chore(web): setup project

--- web/react/src/main.tsx
+++ web/react/src/main.tsx
@@ -2,9 +2,16 @@ import "./style.less";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { WhiteVersion } from "white-web-sdk";
+import { VERSION } from "agora-rtc-sdk"
 
-function App() {
-    return <div>hello world!</div>;
+function App(): React.ReactElement {
+    return <>
+        <pre className="pt2 pl4">
+            using white-web-sdk@{WhiteVersion}<br />
+            using agora-rtc-sdk@{VERSION}
+        </pre>
+    </>;
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
