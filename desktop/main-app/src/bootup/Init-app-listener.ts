74c049cbd6910b468fe61dac89c9cd640685d621
Black-Hole <158blackhole@gmail.com>
2021-05-10 15:14:12 +0800

    refactor(main-app): improve url protocol logic (#619)

--- desktop/main-app/src/bootup/Init-app-listener.ts
+++ desktop/main-app/src/bootup/Init-app-listener.ts
@@ -1,6 +1,6 @@
 import { app } from "electron";
 import closeAPP from "../utils/CloseAPP";
-import { windowManager } from "../utils/WindowManager";
+import { URLProtocol } from "../utils/URLProtocol";
 
 export default () => {
     const windowAllClosed = () => {
@@ -15,45 +15,9 @@ export default () => {
         });
     };
 
-    const macOSopenURL = () => {
-        app.on("open-url", event => {
-            event.preventDefault();
-            const mainWindow = windowManager.getMainWindow()?.window;
-            if (mainWindow) {
-                if (mainWindow.isMinimized()) {
-                    mainWindow.restore();
-                }
-                mainWindow.focus();
-            }
-        });
-    };
-
-    const windowsOpenURL = () => {
-        const lock = app.requestSingleInstanceLock();
-        if (!lock) {
-            app.quit();
-        } else {
-            app.on("second-instance", () => {
-                const mainWindow = windowManager.getMainWindow()?.window;
-                if (mainWindow) {
-                    if (mainWindow.isMinimized()) {
-                        mainWindow.restore();
-                    }
-                    mainWindow.focus();
-                } else {
-                    if (process.platform !== "win32") {
-                        if (app.isReady()) {
-                            windowManager.createMainWindow();
-                        }
-                    }
-                }
-            });
-        }
-    };
-
     // Does not require sequential execution
     // Just to avoid local variables polluting Context variables
-    [windowAllClosed, appQuit, macOSopenURL, windowsOpenURL].forEach(f => {
+    [windowAllClosed, appQuit, URLProtocol].forEach(f => {
         f();
     });
 };
