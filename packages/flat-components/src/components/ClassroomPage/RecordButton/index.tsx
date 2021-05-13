fe1da31b7c87ccc50ae1ff23fd0d3a2a5560f81e
hyrious <hyrious@outlook.com>
2021-05-13 10:43:36 +0800

    fix: typo

--- packages/flat-components/src/components/ClassroomPage/RecordButton/index.tsx
+++ packages/flat-components/src/components/ClassroomPage/RecordButton/index.tsx
@@ -17,7 +17,7 @@ function renderTime(seconds: number): string {
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor((seconds % 3600) % 60);
     return (
-        (h > 0 ? String(s).padStart(2, "0") + ":" : "") +
+        (h > 0 ? String(h).padStart(2, "0") + ":" : "") +
         String(m).padStart(2, "0") +
         ":" +
         String(s).padStart(2, "0")
