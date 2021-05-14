cf15b45956c8f416f06ecb741ee98cba0bddaca1
Black-Hole1 <158blackhole@gmail.com>
2021-05-14 18:17:01 +0800

    refactor(main): use require replace import in electron-devtools-installer and improve dev judge

--- desktop/main-app/src/bootup/Init-window.ts
+++ desktop/main-app/src/bootup/Init-window.ts
@@ -1,7 +1,5 @@
 import { app } from "electron";
-import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 import { windowManager } from "../utils/WindowManager";
-import runtime from "../utils/Runtime";
 
 export default async () => {
     await new Promise(resolve => {
@@ -10,15 +8,16 @@ export default async () => {
 
     app.allowRendererProcessReuse = false;
 
-    if (runtime.isDevelopment) {
+    if (process.env.NODE_ENV === "development") {
+        const installExtension = require("electron-devtools-installer");
         // TODO: replace official package version
         // when writing this code, electron-devtools-installer has not released the latest version
         // Expected version, greater than: v3.1.1
-        installExtension([REACT_DEVELOPER_TOOLS], {
+        installExtension([installExtension.REACT_DEVELOPER_TOOLS], {
             loadExtensionOptions: {
                 allowFileAccess: true,
             },
-        }).catch(err => console.log("added extension failed", err));
+        }).catch((err: Error) => console.log("added extension failed", err));
     }
 
     windowManager.createMainWindow();
