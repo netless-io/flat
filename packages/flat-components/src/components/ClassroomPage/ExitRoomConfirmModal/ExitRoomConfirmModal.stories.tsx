6226fe202d8e2e608d946746e3b9467f532f44ea
CRIMX <straybugs@gmail.com>
2021-05-14 09:52:59 +0800

    refactor(flat-components): update exit-room-confirm-modal (#636)

--- packages/flat-components/src/components/ClassroomPage/ExitRoomConfirmModal/ExitRoomConfirmModal.stories.tsx
+++ packages/flat-components/src/components/ClassroomPage/ExitRoomConfirmModal/ExitRoomConfirmModal.stories.tsx
@@ -1,19 +1,43 @@
 import { Meta, Story } from "@storybook/react";
 import React from "react";
-import { CloseRoomConfirmModal, CloseRoomConfirmModalProps } from ".";
+import {
+    CloseRoomConfirmModal,
+    CloseRoomConfirmModalProps,
+    StopClassConfirmModal,
+    StopClassConfirmModalProps,
+    ExitRoomConfirmModal,
+    ExitRoomConfirmModalProps,
+} from ".";
 
 const storyMeta: Meta = {
     title: "ClassroomPage/CloseRoomConfirmModal",
     component: CloseRoomConfirmModal,
+    parameters: {
+        docs: { page: null },
+    },
 };
 
 export default storyMeta;
 
-export const Overview: Story<CloseRoomConfirmModalProps> = args => (
+export const CloseRoomConfirm: Story<CloseRoomConfirmModalProps> = args => (
     <CloseRoomConfirmModal {...args} />
 );
-Overview.args = {
+CloseRoomConfirm.args = {
     visible: true,
     hangLoading: false,
     stopLoading: false,
+};
+
+export const StopClassConfirm: Story<StopClassConfirmModalProps> = args => (
+    <StopClassConfirmModal {...args} />
+);
+StopClassConfirm.args = {
+    visible: true,
+};
+
+export const ExitRoomConfirm: Story<ExitRoomConfirmModalProps> = args => (
+    <ExitRoomConfirmModal {...args} />
+);
+ExitRoomConfirm.args = {
+    visible: true,
 };
