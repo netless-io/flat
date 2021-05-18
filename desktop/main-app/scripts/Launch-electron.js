d834a4bd852610b77ab5b6ab179b617897ac7007
Black-Hole1 <158blackhole@gmail.com>
2021-05-18 15:39:28 +0800

    fix(electron): cannot launch electron

--- desktop/main-app/scripts/Launch-electron.js
+++ desktop/main-app/scripts/Launch-electron.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const nodemon = require("nodemon");
 
-const distPath = path.resolve(__dirname, "..", "dist", "main", "main.js");
+const distPath = path.resolve(__dirname, "..", "dist", "main.js");
 
 nodemon(`nodemon --watch ${distPath} --exec "electron ${distPath}"`).on("quit", () => {
     process.exit();
