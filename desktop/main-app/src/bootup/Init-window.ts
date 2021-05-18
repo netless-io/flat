f9b8e6c16d8073b28e4d418e3a6c8b06bcabd204
Black-Hole <158blackhole@gmail.com>
2021-05-18 16:58:36 +0800

    fix(electron): error when dev use electron-devtools-installer (#646)

--- desktop/main-app/src/bootup/Init-window.ts
+++ desktop/main-app/src/bootup/Init-window.ts
@@ -13,11 +13,13 @@ export default async () => {
         // TODO: replace official package version
         // when writing this code, electron-devtools-installer has not released the latest version
         // Expected version, greater than: v3.1.1
-        installExtension([installExtension.REACT_DEVELOPER_TOOLS], {
-            loadExtensionOptions: {
-                allowFileAccess: true,
-            },
-        }).catch((err: Error) => console.log("added extension failed", err));
+        installExtension
+            .default([installExtension.REACT_DEVELOPER_TOOLS], {
+                loadExtensionOptions: {
+                    allowFileAccess: true,
+                },
+            })
+            .catch((err: Error) => console.log("added extension failed", err));
     }
 
     windowManager.createMainWindow();
