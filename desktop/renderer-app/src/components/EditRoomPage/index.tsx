f9a536f7e05006bce6684cc5f7a25ee3ca9a1f41
cheerchen <meetcheerego@gmail.com>
2021-05-19 11:43:28 +0800

    refactor(flat-component): remove Link element

--- desktop/renderer-app/src/components/EditRoomPage/index.tsx
+++ desktop/renderer-app/src/components/EditRoomPage/index.tsx
@@ -4,18 +4,18 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 import { EditRoomBody, EditRoomBodyProps, MainPageHeader } from "flat-components";
 import { MainPageLayoutContainer } from "../MainPageLayoutContainer";
-import { generateRoutePath, RouteNameType } from "../../utils/routes";
-import { useLastLocation } from "react-router-last-location";
+import { useHistory } from "react-router";
 
 export type EditRoomPageProps = EditRoomBodyProps;
 
 export const EditRoomPage = observer<EditRoomPageProps>(function EditRoomPage(props) {
-    const lastLocation = useLastLocation();
+    const history = useHistory();
+
     return (
         <MainPageLayoutContainer>
             <div className="edit-room-page">
                 <MainPageHeader
-                    routePath={lastLocation?.pathname || generateRoutePath(RouteNameType.HomePage)}
+                    onBackPreviousPage={() => history.goBack()}
                     title={props.type === "schedule" ? "预定房间" : "修改房间"}
                 />
                 <EditRoomBody {...props} />
