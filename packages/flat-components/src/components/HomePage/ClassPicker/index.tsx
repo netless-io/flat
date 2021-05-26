d794a6d797b241edc6c3d1238b7936720fe39500
Cheerego7 <48879533+Cheerego7@users.noreply.github.com>
2021-05-26 16:32:43 +0800

    feat(flat-component): add large mode to ClassPicker component (#668)

--- packages/flat-components/src/components/HomePage/ClassPicker/index.tsx
+++ packages/flat-components/src/components/HomePage/ClassPicker/index.tsx
@@ -6,11 +6,13 @@ import "./index.less";
 
 import React from "react";
 import { Radio } from "antd";
+import classNames from "classnames";
 
 export type ClassPickerItemType = "oneToOne" | "bigClass" | "smallClass";
 
 export interface ClassPickerProps {
     type: ClassPickerItemType;
+    large?: boolean;
     onChange?: (value: ClassPickerItemType) => void;
 }
 
@@ -32,28 +34,50 @@ const ClassPickerTexts: Record<ClassPickerItemType, string> = {
     smallClass: "适用于 1 位老师面向最多 16 名学生",
 };
 
-export const ClassPickerItem: React.FC<Pick<ClassPickerProps, "type">> = ({ type }) => {
+export const ClassPickerItem: React.FC<Pick<ClassPickerProps, "type" | "large">> = ({
+    type,
+    large,
+}) => {
     return (
         <Radio value={type}>
-            <div className="class-picker-item-container">
-                <img className="class-picker-item-icon" src={ClassPickerIcons[type]}></img>
-                <span className="class-picker-item-title">{ClassPickerTitles[type]}</span>
-                <span className="class-picker-item-content">{ClassPickerTexts[type]}</span>
-            </div>
+            {large ? (
+                <div className="class-large-picker-item-container">
+                    <img
+                        className="class-large-picker-item-icon"
+                        src={ClassPickerIcons[type]}
+                    ></img>
+                    <div className="class-large-picker-item-right">
+                        <span className="class-large-picker-item-title">
+                            {ClassPickerTitles[type]}
+                        </span>
+                        <span className="class-large-picker-item-content">
+                            {ClassPickerTexts[type]}
+                        </span>
+                    </div>
+                </div>
+            ) : (
+                <div className="class-picker-item-container">
+                    <img className="class-picker-item-icon" src={ClassPickerIcons[type]}></img>
+                    <span className="class-picker-item-title">{ClassPickerTitles[type]}</span>
+                    <span className="class-picker-item-content">{ClassPickerTexts[type]}</span>
+                </div>
+            )}
         </Radio>
     );
 };
 
-export const ClassPicker: React.FC<ClassPickerProps> = ({ type, onChange }) => {
+export const ClassPicker: React.FC<ClassPickerProps> = ({ type, onChange, large }) => {
     return (
         <Radio.Group
-            className="class-picker-container"
+            className={classNames("class-picker-container", {
+                "class-large-picker-container": large,
+            })}
             value={type}
             onChange={e => onChange && e.target.value && onChange(e.target.value)}
         >
-            <ClassPickerItem type="bigClass" />
-            <ClassPickerItem type="smallClass" />
-            <ClassPickerItem type="oneToOne" />
+            <ClassPickerItem type="bigClass" large={large} />
+            <ClassPickerItem type="smallClass" large={large} />
+            <ClassPickerItem type="oneToOne" large={large} />
         </Radio.Group>
     );
 };
