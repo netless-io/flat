ecf6ebd3506f99226c736cf03bc0ce260a35d5ac
BlackHole1 <158blackhole@gmail.com>
2021-05-26 13:54:36 +0800

    chore(ci): optimization code check ci speed

--- scripts/build-packages.js
+++ scripts/build-packages.js
@@ -5,12 +5,26 @@ const shell = require("shelljs");
 // Set commands context to project root
 shell.cd(path.join(__dirname, ".."));
 
-const packages = fs
-    .readdirSync(path.join(__dirname, "..", "packages"))
-    .filter(name => !name.startsWith("."));
+const packages = (() => {
+    // if the current environment is CI, the package to be compiled is selected by reading the workspaces.packages information under package.json.
+    // because in a CI environment, we will first call the install-changed-package-dependencies.js script to selectively install which package dependencies
+    if (process.env.CI) {
+        const rootPackageJSON = require(path.join(__dirname, "..", "package.json"));
+        const prefixPath = "packages/";
 
-for (const package of packages) {
-    const pkgPath = path.join(__dirname, "..", "packages", package);
+        // ["packages/flat-type", "desktop/main-app"] => ["flat-type"]
+        return rootPackageJSON.workspaces.packages
+            .filter(packagePath => packagePath.startsWith(prefixPath))
+            .map(packagePath => packagePath.slice(prefixPath.length));
+    }
+
+    return fs
+        .readdirSync(path.join(__dirname, "..", "packages"))
+        .filter(name => !name.startsWith("."));
+})();
+
+for (const packageName of packages) {
+    const pkgPath = path.join(__dirname, "..", "packages", packageName);
     const pkgJSONPath = path.join(pkgPath, "package.json");
 
     if (fs.existsSync(pkgJSONPath)) {
@@ -18,7 +32,7 @@ for (const package of packages) {
 
         // some packages may not have build command
         if (pkgJSON && pkgJSON.scripts && pkgJSON.scripts.build) {
-            console.log(`\nbuilding "/packages/${package}"\n`);
+            console.log(`\nbuilding "/packages/${packageName}"\n`);
 
             const buildExitCode = shell.exec(`yarn --cwd "${pkgPath}" build`).code;
             if (buildExitCode !== 0) {
---
+++ scripts/ci/install-changed-package-dependencies.js
@@ -0,0 +1,53 @@
+if (!process.env.CI) {
+    return;
+}
+
+const fs = require("fs");
+const path = require("path");
+
+const { components, types, renderer, main, flatWeb } = process.env;
+
+const isTrue = v => v === "true";
+
+let workspaces = [];
+
+if (isTrue(types)) {
+    workspaces.push("packages/flat-types");
+}
+
+if (isTrue(components)) {
+    workspaces.push("packages/flat-types", "packages/flat-components");
+}
+
+if (isTrue(renderer)) {
+    workspaces.push("packages/flat-types", "packages/flat-components", "packages/renderer-app");
+}
+
+if (isTrue(main)) {
+    workspaces.push("packages/flat-types", "desktop/main-app");
+}
+
+if (isTrue(flatWeb)) {
+    workspaces.push("packages/flat-types", "packages/flat-components", "web/flat-web");
+}
+
+workspaces = Array.from(new Set(workspaces));
+
+// if there are no changes, there is no need to install any dependencies
+if (workspaces.length === 0) {
+    console.log("no changed, no need install dependencies");
+    process.exit(1);
+}
+
+console.log(`will install dependencies in: ${workspaces.join(", ")}`);
+
+const packageJSONFilePath = path.join(__dirname, "..", "..", "package.json");
+const packageJSONFileContent = require(packageJSONFilePath);
+
+packageJSONFileContent.workspaces.packages = workspaces;
+
+const newFileContent = JSON.stringify(packageJSONFileContent, null, 2);
+
+fs.writeFileSync(packageJSONFilePath, newFileContent, {
+    encoding: "utf-8",
+});
