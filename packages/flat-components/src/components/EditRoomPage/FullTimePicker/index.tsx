e43afc2803a1ae1bc5ab49d02fe66bcbe89f5dfd
crimx <straybugs@gmail.com>
2021-05-28 15:54:52 +0800

    refactor(flat-components): pack external less

--- packages/flat-components/src/components/EditRoomPage/FullTimePicker/index.tsx
+++ packages/flat-components/src/components/EditRoomPage/FullTimePicker/index.tsx
@@ -1,11 +1,12 @@
 // https://ant.design/docs/react/replace-moment
 
+import "./style.less";
+
 import React, { FC, useContext } from "react";
 import { Col, Row } from "antd";
 import dateFnsGenerateConfig from "rc-picker/lib/generate/dateFns";
 import generatePicker, { PickerTimeProps, PickerProps } from "antd/es/date-picker/generatePicker";
 import { ConfigContext } from "antd/lib/config-provider";
-import "antd/es/date-picker/style/index";
 
 export type DatePickerProps = PickerProps<Date>;
 
