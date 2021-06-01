c477b8d8af3b29bdad41ef55324874fc47edbf2f
crimx <straybugs@gmail.com>
2021-06-01 10:39:31 +0800

    refactor(flat-components): split MainPageLayout

---
+++ packages/flat-components/src/components/MainPageLayout/MainPageNavAvatar/MainPageNavAvatar.stories.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/display-name */
+import { Meta, Story } from "@storybook/react";
+import React, { PropsWithChildren } from "react";
+import { MainPageNavAvatar, MainPageNavAvatarProps } from ".";
+import { CloudOutlined } from "@ant-design/icons";
+
+const storyMeta: Meta = {
+    title: "MainPageLayout/MainPageNavAvatar",
+    component: MainPageNavAvatar,
+    parameters: {
+        layout: "fullscreen",
+    },
+};
+
+export default storyMeta;
+
+export const Overview: Story<PropsWithChildren<MainPageNavAvatarProps>> = args => (
+    <div className="vh-100 pa3">
+        <MainPageNavAvatar {...args} />
+    </div>
+);
+Overview.args = {
+    popMenu: [
+        {
+            key: "userConfig",
+            icon: () => <CloudOutlined />,
+            title: "个人设置",
+            route: "/config",
+        },
+        {
+            key: "getGitHubCode",
+            icon: () => <CloudOutlined />,
+            title: "获取源码",
+            route: "/github",
+        },
+        {
+            key: "logout",
+            icon: () => <CloudOutlined className="red" />,
+            title: <span className="red">退出登录</span>,
+            route: "/logout",
+        },
+    ],
+    activeKeys: ["home"],
+    avatarSrc: "http://placekitten.com/200/200",
+    userName: "Flat Name",
+};
+Overview.argTypes = {
+    activeKeys: {
+        control: {
+            type: "multi-select",
+            options: ["home", "cloudStorage", "deviceCheck"],
+        },
+    },
+};
