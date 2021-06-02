ea7e2006d6d426382c327b9a25318e2a4b47c024
cheerchen <meetcheerego@gmail.com>
2021-06-02 10:45:03 +0800

    feat(flat-component): add new nav component to MainPageLayout

---
+++ packages/flat-components/src/components/MainPageLayout/MainPageNavHorizontal/MainPageNavHorizontal.stories.tsx
@@ -0,0 +1,100 @@
+/* eslint-disable react/display-name */
+import { Meta, Story } from "@storybook/react";
+import React, { PropsWithChildren } from "react";
+import { MainPageNavHorizontal, MainPageNavHorizontalProps } from ".";
+import {
+    CloudFilled,
+    CloudOutlined,
+    HomeFilled,
+    HomeOutlined,
+    ToolFilled,
+    ToolOutlined,
+} from "@ant-design/icons";
+
+const storyMeta: Meta = {
+    title: "MainPageLayout/MainPageNavHorizontal",
+    component: MainPageNavHorizontal,
+    parameters: {
+        layout: "fullscreen",
+    },
+};
+
+export default storyMeta;
+
+const sideMenuStyles: React.CSSProperties = { fontSize: 25 };
+
+export const Overview: Story<PropsWithChildren<MainPageNavHorizontalProps>> = args => (
+    <div className="vh-100 pa3">
+        <MainPageNavHorizontal {...args} />
+    </div>
+);
+Overview.args = {
+    leftMenu: [
+        {
+            key: "home",
+            icon: active =>
+                active ? (
+                    <HomeFilled style={sideMenuStyles} />
+                ) : (
+                    <HomeOutlined style={sideMenuStyles} />
+                ),
+            title: "首页",
+            route: "/home",
+        },
+        {
+            key: "cloudStorage",
+            icon: active =>
+                active ? (
+                    <CloudFilled style={sideMenuStyles} />
+                ) : (
+                    <CloudOutlined style={sideMenuStyles} />
+                ),
+            title: "云盘",
+            route: "/cloudStorage",
+        },
+    ],
+    rightMenu: [
+        {
+            key: "deviceCheck",
+            icon: active =>
+                active ? (
+                    <ToolFilled style={sideMenuStyles} />
+                ) : (
+                    <ToolOutlined style={sideMenuStyles} />
+                ),
+            title: "deviceCheck",
+            route: "/deviceCheck",
+        },
+    ],
+    popMenu: [
+        {
+            key: "userConfig",
+            icon: () => <CloudOutlined />,
+            title: "个人设置",
+            route: "/config",
+        },
+        {
+            key: "getGitHubCode",
+            icon: () => <CloudOutlined />,
+            title: "获取源码",
+            route: "/github",
+        },
+        {
+            key: "logout",
+            icon: () => <CloudOutlined className="red" />,
+            title: <span className="red">退出登录</span>,
+            route: "/logout",
+        },
+    ],
+    activeKeys: ["home"],
+    avatarSrc: "http://placekitten.com/200/200",
+    userName: "Flat Name",
+};
+Overview.argTypes = {
+    activeKeys: {
+        control: {
+            type: "multi-select",
+            options: ["home", "cloudStorage", "deviceCheck"],
+        },
+    },
+};
