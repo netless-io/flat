2b67286eed8ab3c986a6f345dded94340d1d2a7b
hyrious <hyrious@outlook.com>
2021-06-03 14:15:23 +0800

    chore(flat-web): add dotenv plugin to skip `VITE_*` restriction (#699)
    * chore(flat-web): add dotenv plugin to skip `VITE_*` restriction
    
    * chore(flat-components): fix missing @types/react-router-dom

---
+++ web/flat-web/scripts/vite-dotenv-plugin.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+import dotenvReal from "dotenv";
+import dotenvExpand from "dotenv-expand";
+import type { Plugin } from "vite";
+
+// based on https://github.com/IndexXuan/vite-plugin-env-compatible
+export function dotenv(envDir: string = process.cwd()): Plugin {
+    return {
+        name: "flat:dotenv",
+        enforce: "pre",
+        config(config, { mode }) {
+            const targetFile = path.join(envDir, `.env.${mode}.local`);
+            if (fs.existsSync(targetFile) && fs.statSync(targetFile).isFile()) {
+                const parsed = dotenvReal.parse(fs.readFileSync(targetFile));
+                dotenvExpand({ parsed });
+                const env = { ...parsed };
+                const define: Record<string, string | {}> = {};
+                for (const [key, value] of Object.entries(env)) {
+                    define[`import.meta.env.${key}`] = JSON.stringify(value);
+                }
+                config.define = { ...config.define, ...define };
+            }
+        },
+    };
+}
