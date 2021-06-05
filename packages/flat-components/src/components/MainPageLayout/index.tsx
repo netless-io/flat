4aa44e178e7b2962b9de5dfe713d7e41c0a8441a
cheerchen <meetcheerego@gmail.com>
2021-06-05 15:44:13 +0800

    refactor(flat-component): split subMenu component.

--- packages/flat-components/src/components/MainPageLayout/index.tsx
+++ packages/flat-components/src/components/MainPageLayout/index.tsx
@@ -1,19 +1,19 @@
 import "./style.less";
 import React from "react";
-import classNames from "classnames";
 import { MainPageLayoutItem } from "./types";
 import { MainPageNav, MainPageNavProps } from "./MainPageNav";
+import { MainPageSubMenu, MainPageSubMenuProps } from "./MainPageSubMenu";
 
 export * from "./MainPageHeader";
 export type { MainPageLayoutItem } from "./types";
 
-export interface MainPageLayoutProps extends MainPageNavProps {
+export interface MainPageLayoutProps
+    extends MainPageNavProps,
+        Partial<Omit<MainPageSubMenuProps, "onClick" | "activeKeys">> {
     /** when an item is clicked */
     onClick: (mainPageLayoutItem: MainPageLayoutItem) => void;
     /** a list of keys to highlight the items */
     activeKeys: string[];
-    /** inside sub menu in MainPageLayout */
-    subMenu?: MainPageLayoutItem[];
 }
 
 export const MainPageLayout: React.FC<MainPageLayoutProps> = ({
@@ -27,30 +27,7 @@ export const MainPageLayout: React.FC<MainPageLayoutProps> = ({
         <div className="main-layout-container">
             <MainPageNav {...restProps} onClick={onClick} activeKeys={activeKeys} />
             {subMenu && (
-                <div className="main-layout-sub-menu-container">
-                    <ul>
-                        {subMenu.map(menuItem => {
-                            return (
-                                <li key={menuItem.key}>
-                                    <a
-                                        className={classNames("main-layout-sub-menu-item", {
-                                            "is-active": activeKeys.includes(menuItem.key),
-                                        })}
-                                        onClick={e => {
-                                            e.preventDefault();
-                                            onClick(menuItem);
-                                        }}
-                                    >
-                                        <span className="main-layout-sub-menu-item-icon">
-                                            {menuItem.icon(activeKeys.includes(menuItem.key))}
-                                        </span>
-                                        {menuItem.title}
-                                    </a>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
+                <MainPageSubMenu onClick={onClick} activeKeys={activeKeys} subMenu={subMenu} />
             )}
             <div className="main-layout-container-content fancy-scrollbar">{children}</div>
         </div>
--- packages/flat-components/src/components/MainPageLayoutHorizontal/index.tsx
+++ packages/flat-components/src/components/MainPageLayoutHorizontal/index.tsx
@@ -1,19 +1,19 @@
 import "./style.less";
 import React from "react";
-import classNames from "classnames";
 import { MainPageLayoutItem } from "../MainPageLayout/types";
 import {
     MainPageNavHorizontal,
     MainPageNavHorizontalProps,
 } from "../MainPageLayout/MainPageNavHorizontal";
+import { MainPageSubMenu, MainPageSubMenuProps } from "../MainPageLayout/MainPageSubMenu";
 
-export interface MainPageLayoutHorizontalProps extends MainPageNavHorizontalProps {
+export interface MainPageLayoutHorizontalProps
+    extends MainPageNavHorizontalProps,
+        Partial<Omit<MainPageSubMenuProps, "onClick" | "activeKeys">> {
     /** when an item is clicked */
     onClick: (mainPageLayoutItem: MainPageLayoutItem) => void;
     /** a list of keys to highlight the items */
     activeKeys: string[];
-    /** inside sub menu in MainPageLayout */
-    subMenu?: MainPageLayoutItem[];
 }
 
 export const MainPageLayoutHorizontal: React.FC<MainPageLayoutHorizontalProps> = ({
@@ -26,34 +26,19 @@ export const MainPageLayoutHorizontal: React.FC<MainPageLayoutHorizontalProps> =
     return (
         <div className="main-horizontal-layout-container">
             <MainPageNavHorizontal {...restProps} onClick={onClick} activeKeys={activeKeys} />
-            {subMenu && (
-                <div className="main-horizontal-layout-sub-menu-container">
-                    <ul>
-                        {subMenu.map(menuItem => {
-                            return (
-                                <li key={menuItem.key}>
-                                    <a
-                                        className={classNames("main-layout-sub-menu-item", {
-                                            "is-active": activeKeys.includes(menuItem.key),
-                                        })}
-                                        onClick={e => {
-                                            e.preventDefault();
-                                            onClick(menuItem);
-                                        }}
-                                    >
-                                        <span className="main-layout-sub-menu-item-icon">
-                                            {menuItem.icon(activeKeys.includes(menuItem.key))}
-                                        </span>
-                                        {menuItem.title}
-                                    </a>
-                                </li>
-                            );
-                        })}
-                    </ul>
+            <div className="main-horizontal-layout-outside-wrapper">
+                <div className="main-horizontal-layout-content-wrapper">
+                    {subMenu && (
+                        <MainPageSubMenu
+                            onClick={onClick}
+                            activeKeys={activeKeys}
+                            subMenu={subMenu}
+                        />
+                    )}
+                    <div className="main-horizontal-layout-container-content fancy-scrollbar">
+                        {children}
+                    </div>
                 </div>
-            )}
-            <div className="main-horizontal-layout-container-content fancy-scrollbar">
-                {children}
             </div>
         </div>
     );
