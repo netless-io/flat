b408fe249c11b02170d9d8e1b87660e05b65d036
Cheerego7 <48879533+Cheerego7@users.noreply.github.com>
2021-06-05 16:24:45 +0800

    refactor(flat-component): split subMenu component. (#706)

---
+++ packages/flat-components/src/components/MainPageLayout/MainPageSubMenu/index.tsx
@@ -0,0 +1,47 @@
+import "./style.less";
+
+import React from "react";
+import classNames from "classnames";
+import { MainPageLayoutItem } from "../types";
+
+export interface MainPageSubMenuProps {
+    /** when an item is clicked */
+    onClick: (mainPageLayoutItem: MainPageLayoutItem) => void;
+    /** a list of keys to highlight the items */
+    activeKeys: string[];
+    /** inside sub menu in MainPageLayout */
+    subMenu: MainPageLayoutItem[];
+}
+
+export const MainPageSubMenu: React.FC<MainPageSubMenuProps> = ({
+    subMenu,
+    onClick,
+    activeKeys,
+}) => {
+    return (
+        <div className="main-layout-sub-menu-container">
+            <ul>
+                {subMenu.map(menuItem => {
+                    return (
+                        <li key={menuItem.key}>
+                            <a
+                                className={classNames("main-layout-sub-menu-item", {
+                                    "is-active": activeKeys.includes(menuItem.key),
+                                })}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    onClick(menuItem);
+                                }}
+                            >
+                                <span className="main-layout-sub-menu-item-icon">
+                                    {menuItem.icon(activeKeys.includes(menuItem.key))}
+                                </span>
+                                {menuItem.title}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
---
+++ packages/flat-components/src/components/MainPageLayoutHorizontal/MainPageLayoutHorizontal.stories.tsx
@@ -0,0 +1,177 @@
+/* eslint-disable react/display-name */
+import { Meta, Story } from "@storybook/react";
+import React, { PropsWithChildren } from "react";
+import { MainPageLayoutHorizontal, MainPageLayoutHorizontalProps } from ".";
+import {
+    CloudFilled,
+    CloudOutlined,
+    HomeFilled,
+    HomeOutlined,
+    ToolFilled,
+    ToolOutlined,
+} from "@ant-design/icons";
+import faker from "faker";
+
+const storyMeta: Meta = {
+    title: "MainPageLayout/MainPageLayoutHorizontal",
+    component: MainPageLayoutHorizontal,
+};
+
+export default storyMeta;
+
+const sideMenuStyles: React.CSSProperties = { fontSize: 25 };
+
+export const Overview: Story<PropsWithChildren<MainPageLayoutHorizontalProps>> = args => (
+    <div className="vh-75 mw8-ns">
+        <MainPageLayoutHorizontal {...args} />
+    </div>
+);
+Overview.args = {
+    leftMenu: [
+        {
+            key: "home",
+            icon: active =>
+                active ? (
+                    <HomeFilled style={sideMenuStyles} />
+                ) : (
+                    <HomeOutlined style={sideMenuStyles} />
+                ),
+            title: "home",
+            route: "/home",
+        },
+        {
+            key: "cloudStorage",
+            icon: active =>
+                active ? (
+                    <CloudFilled style={sideMenuStyles} />
+                ) : (
+                    <CloudOutlined style={sideMenuStyles} />
+                ),
+            title: "cloudStorage",
+            route: "/cloudStorage",
+        },
+    ],
+    rightMenu: [
+        {
+            key: "deviceCheck",
+            icon: active =>
+                active ? (
+                    <ToolFilled style={sideMenuStyles} />
+                ) : (
+                    <ToolOutlined style={sideMenuStyles} />
+                ),
+            title: "deviceCheck",
+            route: "/deviceCheck",
+        },
+    ],
+    popMenu: [
+        {
+            key: "userConfig",
+            icon: () => <CloudOutlined />,
+            title: "个人设置",
+            route: "/config",
+        },
+        {
+            key: "getGitHubCode",
+            icon: () => <CloudOutlined />,
+            title: "获取源码",
+            route: "/github",
+        },
+        {
+            key: "logout",
+            icon: () => <CloudOutlined className="red" />,
+            title: <span className="red">退出登录</span>,
+            route: "/logout",
+        },
+    ],
+    activeKeys: ["home"],
+    avatarSrc: "http://placekitten.com/200/200",
+    children: <div>content</div>,
+    userName: "Flat Name",
+};
+Overview.argTypes = {
+    activeKeys: {
+        control: {
+            type: "multi-select",
+            options: ["home", "cloudStorage", "deviceCheck"],
+        },
+    },
+};
+
+export const WithSubMenu: Story<MainPageLayoutHorizontalProps> = args => (
+    <div className="vh-75 mw8-ns">
+        <MainPageLayoutHorizontal {...args} />
+    </div>
+);
+WithSubMenu.args = {
+    ...Overview.args,
+    subMenu: [
+        {
+            key: "systemTesting",
+            icon: () => <ToolOutlined />,
+            title: "系统检测",
+            route: "/device/system",
+        },
+        {
+            key: "cameraTesting",
+            icon: () => <CloudOutlined />,
+            title: "摄像头检测",
+            route: "/device/camera",
+        },
+        {
+            key: "speakerTesting",
+            icon: () => <CloudOutlined />,
+            title: "扬声器检测",
+            route: "/device/speaker",
+        },
+        {
+            key: "microphoneTesting",
+            icon: () => <CloudOutlined />,
+            title: "麦克风检测",
+            route: "/device/microphone",
+        },
+    ],
+    activeKeys: ["systemTesting"],
+};
+
+export const LongContent: Story<MainPageLayoutHorizontalProps> = args => (
+    <div className="vh-75 mw8-ns">
+        <MainPageLayoutHorizontal {...args}>
+            {Array(300)
+                .fill(0)
+                .map(() => (
+                    <p>{faker.random.words()}</p>
+                ))}
+        </MainPageLayoutHorizontal>
+    </div>
+);
+LongContent.args = {
+    ...Overview.args,
+    subMenu: [
+        {
+            key: "systemTesting",
+            icon: () => <ToolOutlined />,
+            title: "系统检测",
+            route: "/device/system",
+        },
+        {
+            key: "cameraTesting",
+            icon: () => <CloudOutlined />,
+            title: "摄像头检测",
+            route: "/device/camera",
+        },
+        {
+            key: "speakerTesting",
+            icon: () => <CloudOutlined />,
+            title: "扬声器检测",
+            route: "/device/speaker",
+        },
+        {
+            key: "microphoneTesting",
+            icon: () => <CloudOutlined />,
+            title: "麦克风检测",
+            route: "/device/microphone",
+        },
+    ],
+    activeKeys: ["systemTesting"],
+};
