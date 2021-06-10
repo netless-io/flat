a00ab0758e4160c83b65f187de1903871063cd31
hyrious <hyrious@outlook.com>
2021-06-10 18:21:16 +0800

    fix(flat-web): wrong assets url in build result

--- web/flat-web/src/components/TopBarRoundBtn/index.tsx
+++ web/flat-web/src/components/TopBarRoundBtn/index.tsx
@@ -1,6 +1,8 @@
 import { TopBarRoundBtn as TopBarRoundBtnImpl } from "flat-components";
 import React, { ReactElement } from "react";
 
+const Icons = import.meta.globEager("./icons/*.svg");
+
 export interface TopBarRoundBtnProps extends React.HTMLAttributes<HTMLButtonElement> {
     dark?: boolean;
     /** file name (without .ext) in src/assets/image/ */
@@ -16,7 +18,7 @@ export function TopBarRoundBtn({
         <TopBarRoundBtnImpl
             {...restProps}
             dark={dark}
-            icon={<img src={`/src/assets/image/${iconName}.svg`} />}
+            icon={<img src={Icons[`./icons/${iconName}.svg`].default} />}
         />
     );
 }
