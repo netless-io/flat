cee89b88d38c35378bd989e0524bffff265cd3a9
Leooeloel <1826234293@qq.com>
2021-06-16 10:03:05 +0800

    feat(i18n): AppUpgradeModal、CloudStorageContainer

--- desktop/renderer-app/src/components/AppUpgradeModal/index.tsx
+++ desktop/renderer-app/src/components/AppUpgradeModal/index.tsx
@@ -3,6 +3,7 @@ import { Button, Modal } from "antd";
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
 import { ipcAsyncByMainWindow, ipcReceive, ipcReceiveRemove } from "../../utils/ipc";
+import { useTranslation } from "react-i18next";
 
 export interface AppUpgradeModalProps {
     /** open modal when newVersion has truthy value */
@@ -15,6 +16,7 @@ export const AppUpgradeModal = observer<AppUpgradeModalProps>(function AppUpgrad
     newVersion,
     onClose,
 }) {
+    const { t } = useTranslation();
     const [upgradePercent, setUpgradePercent] = useState(0);
     const [showUpgradeProgress, setShowUpgradeProgress] = useState(false);
     const [upgradeFail, setUpgradeFail] = useState(false);
@@ -37,7 +39,7 @@ export const AppUpgradeModal = observer<AppUpgradeModalProps>(function AppUpgrad
     }, [showUpgradeProgress]);
 
     const renderModalTitle = (): React.ReactNode => {
-        return <div className="app-upgrade-modal-title">版本更新</div>;
+        return <div className="app-upgrade-modal-title">{t("version-updates")}</div>;
     };
 
     const cancelUpgrade = (): void => {
@@ -64,7 +66,7 @@ export const AppUpgradeModal = observer<AppUpgradeModalProps>(function AppUpgrad
             {showUpgradeProgress ? (
                 <div>
                     <span className="app-upgrade-modal-font">
-                        下载中 ({upgradePercent.toFixed(2)}%)...
+                        {t("downloading")} ({upgradePercent.toFixed(2)}%)...
                     </span>
                     <div className="app-upgrade-modal-progress"></div>
                     <div
@@ -73,10 +75,12 @@ export const AppUpgradeModal = observer<AppUpgradeModalProps>(function AppUpgrad
                     ></div>
                     {upgradeFail && (
                         <div>
-                            <span className="app-upgrade-modal-font">更新失败，请重新打开程序</span>
+                            <span className="app-upgrade-modal-font">
+                                {t("update-failed-tips")}
+                            </span>
                             <div className="app-upgrade-modal-btn">
                                 <Button type="primary" onClick={() => window.close()}>
-                                    确定
+                                    {t("confirm")}
                                 </Button>
                             </div>
                         </div>
@@ -85,11 +89,11 @@ export const AppUpgradeModal = observer<AppUpgradeModalProps>(function AppUpgrad
             ) : (
                 <div>
                     <span className="app-upgrade-modal-font">
-                        发现新版本{newVersion || " "}, 请更新到最新版本获取更好的产品体验
+                        {t("new-version-tips", { version: newVersion || " " })}
                     </span>
                     <div className="app-upgrade-modal-btn">
                         <Button type="primary" onClick={upgradeStart}>
-                            立即更新
+                            {t("update-now")}
                         </Button>
                     </div>
                 </div>
