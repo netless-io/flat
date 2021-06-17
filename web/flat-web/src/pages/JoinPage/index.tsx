9ce795f0ee4770277e431c0c0a92b92a7e571712
Cheerego7 <48879533+Cheerego7@users.noreply.github.com>
2021-06-17 20:18:34 +0800

    refactor(flat-web): use active to launch electron (#752)

--- web/flat-web/src/pages/JoinPage/index.tsx
+++ web/flat-web/src/pages/JoinPage/index.tsx
@@ -40,7 +40,7 @@ export const JoinPage = observer(function JoinPage() {
 
         checkLogin();
 
-        window.location.href = "x-agora-flat-client://open";
+        window.location.href = "x-agora-flat-client://active";
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
@@ -107,7 +107,7 @@ export const JoinPage = observer(function JoinPage() {
                 </div>
                 <div className="join-page-content-container-open-flat">
                     <span>已经安装 Flat ? </span>
-                    <a href="x-agora-flat-client://open">立即打开</a>
+                    <a href="x-agora-flat-client://active">立即打开</a>
                 </div>
             </div>
             <div className="join-page-footer-container">
