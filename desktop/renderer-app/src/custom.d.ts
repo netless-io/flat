ff6b16d40fd33b30c764f28836d0f859410e1a02
RISHABH BUDHIRAJA <rishabh.budhiraja@gmail.com>
2021-06-26 22:19:14 +0530

    Revert "Cleanup: Removes Unused files"
    This reverts commit 61d38b4241b3e3026b4d741ec6378d20b08f264f.

---
+++ desktop/renderer-app/src/custom.d.ts
@@ -0,0 +1,14 @@
+declare module "*.svg" {
+    const content: string;
+    export = content;
+}
+
+declare module "*.jpg" {
+    const content: string;
+    export = content;
+}
+
+declare module "*.png" {
+    const content: string;
+    export = content;
+}
