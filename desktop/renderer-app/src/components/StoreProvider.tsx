071f43bb1ad690a36876e04421a8cf3e6a00c1c6
Cheerego7 <48879533+Cheerego7@users.noreply.github.com>
2021-06-29 10:58:19 +0800

    feat(URLProtocol): listen to ipc event for launch flat app (#768)

--- desktop/renderer-app/src/components/StoreProvider.tsx
+++ desktop/renderer-app/src/components/StoreProvider.tsx
@@ -2,6 +2,7 @@ import React, { createContext, FC } from "react";
 import { configStore } from "../stores/ConfigStore";
 import { globalStore } from "../stores/GlobalStore";
 import { roomStore } from "../stores/RoomStore";
+import { urlProtocolStore } from "../stores/URLProtocolStore";
 
 export const GlobalStoreContext = createContext(globalStore);
 
@@ -9,10 +10,14 @@ export const RoomStoreContext = createContext(roomStore);
 
 export const ConfigStoreContext = createContext(configStore);
 
+export const URLProtocolStoreContext = createContext(urlProtocolStore);
+
 export const StoreProvider: FC = ({ children }) => (
     <GlobalStoreContext.Provider value={globalStore}>
-        <ConfigStoreContext.Provider value={configStore}>
-            <RoomStoreContext.Provider value={roomStore}>{children}</RoomStoreContext.Provider>
-        </ConfigStoreContext.Provider>
+        <URLProtocolStoreContext.Provider value={urlProtocolStore}>
+            <ConfigStoreContext.Provider value={configStore}>
+                <RoomStoreContext.Provider value={roomStore}>{children}</RoomStoreContext.Provider>
+            </ConfigStoreContext.Provider>
+        </URLProtocolStoreContext.Provider>
     </GlobalStoreContext.Provider>
 );
