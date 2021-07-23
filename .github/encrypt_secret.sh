fea8182e8111a10e50b8475b59a27f205a02f415
siyu <chensiyi@agora.io>
2021-07-23 19:29:35 +0800

    add github actions to build flat-web image

---
+++ .github/encrypt_secret.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# github actions doc: https://docs.github.com/en/actions/reference/encrypted-secrets
+
+# Prepare:
+#   install gpg
+#
+#   macos: brew install gpg
+#   ubuntu/debian: apt-get install -y gpg
+#   centos/RedHat: apt-get install -y gpg
+#
+# Usage:
+#
+# ./.github/envcrypt_secret.sh [development|production]
+
+set -e;
+
+ENV=$1
+if [ -f "config/.env.${ENV}.local" ]
+then
+    echo "encrypt config/.env.${ENV}.local to base64 format"
+    gpg --symmetric  --output tmp --cipher-algo AES256 config/.env.${ENV}.local
+    cat tmp | base64 -w 0 > config/.env.${ENV}.local.gpg
+    rm tmp
+    echo "encrypt complete, add .env.${ENV}.local.gpg to github secret >>GPG_SECRET_FILE<< and PASSWORD to github >>GPG_SECRET_PASSWORD<< for flat-web docker build"
+else
+    echo "Cannot find config/.env.${ENV}.local"
+fi
