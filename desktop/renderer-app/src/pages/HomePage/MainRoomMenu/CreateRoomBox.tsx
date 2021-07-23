c1aba0362dbb117a0132031f593e4dded33ed49c
Leo <1826234293@qq.com>
2021-07-23 10:34:49 +0800

    feat(i18n): add server i18n (#813)

--- desktop/renderer-app/src/pages/HomePage/MainRoomMenu/CreateRoomBox.tsx
+++ desktop/renderer-app/src/pages/HomePage/MainRoomMenu/CreateRoomBox.tsx
@@ -64,7 +64,7 @@ export const CreateRoomBox = observer<CreateRoomBoxProps>(function CreateRoomBox
             style={{ width: "auto" }}
             onClick={e => setRoomRegion(e.key as Region)}
         >
-            <div style={{ padding: "4px 12px 0 14px", color: "gray" }}>服务器</div>
+            <div style={{ padding: "4px 12px 0 14px", color: "gray" }}>{t("servers")}</div>
             {regions.map(region => (
                 <Menu.Item key={region}>
                     <img src={RegionSVG[region]} alt={region} style={{ width: 22 }} />
