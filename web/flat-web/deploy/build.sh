908521d0b3efe531b66f4df876be13c126eb8152
siyu <chensiyi@agora.io>
2021-07-26 16:47:44 +0800

    fix dockerfile path

--- web/flat-web/deploy/build.sh
+++ web/flat-web/deploy/build.sh
@@ -6,4 +6,4 @@ export TAG=${TAG-:latest}
 export ENV=${ENV-:development}
 
 
-docker build . -f deploy/Dockerfile -t agoraflat/flat-web:$TAG --build-arg build_env=$ENV --ulimit nofile=65535:65535
+docker build . -f web/flat-web/deploy/Dockerfile -t agoraflat/flat-web:$TAG --build-arg build_env=$ENV --ulimit nofile=65535:65535
