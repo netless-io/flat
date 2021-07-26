42aa4eed7640618ab93e9fa6370adffbf2359a76
siyu <chensiyi@agora.io>
2021-07-26 17:10:54 +0800

    docker push

--- web/flat-web/deploy/build.sh
+++ web/flat-web/deploy/build.sh
@@ -7,3 +7,4 @@ export ENV=${ENV-:development}
 
 
 docker build . -f web/flat-web/deploy/Dockerfile -t agoraflat/flat-web:$TAG --build-arg build_env=$ENV --ulimit nofile=65535:65535
+docker push agoraflat/flat-web:$TAG
