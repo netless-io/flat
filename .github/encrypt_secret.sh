f356400c84881c5b5135817a0c2b873010cd2b71
siyu <chensiyi@agora.io>
2021-07-28 15:17:48 +0800

    fix

--- .github/encrypt_secret.sh
+++ .github/encrypt_secret.sh
@@ -16,13 +16,21 @@
 set -e;
 
 ENV=$1
+
+if [ -d $ENV ]
+then
+    echo "Cannot find args ENV"
+    echo "Usage: ./.github/envcrypt_secret.sh [development|production]"
+    exit 1
+fi
+
 if [ -f "config/.env.${ENV}.local" ]
 then
     echo "encrypt config/.env.${ENV}.local to base64 format"
-    gpg --symmetric  --output tmp --cipher-algo AES256 config/.env.${ENV}.local
+    gpg --symmetric --output tmp --cipher-algo AES256 config/.env.${ENV}.local
     cat tmp | base64 -w 0 > config/.env.${ENV}.local.gpg
     rm tmp
-    echo "encrypt complete, add .env.${ENV}.local.gpg to github secret >>GPG_SECRET_FILE<< and PASSWORD to github >>GPG_SECRET_PASSWORD<< for flat-web docker build"
+    echo "encrypt complete, add .env.${ENV}.local.gpg to github secret >>GPG_SECRET_FILE<< and PASSWORD to github >>GPG_SECRET_PASSWORD<< for flat docker build"
 else
     echo "Cannot find config/.env.${ENV}.local"
 fi
--- web/flat-web/deploy/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e;
-
-export TAG=${TAG-:latest}
-
-export ENV=${ENV-:development}
-
-
-docker build . -f web/flat-web/deploy/Dockerfile -t agoraflat/flat-web:$TAG --build-arg build_env=$ENV --ulimit nofile=65535:65535
