a4e43c7b16578b991b516d8e4aba8bb8d732163b
Black-Hole <158blackhole@gmail.com>
2021-08-17 11:30:45 +0800

    chore(prettier): replace single quote with double quote (#878)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    'printWidth': 100,
-    'tabWidth': 4,
-    'useTabs': false,
-    'semi': true,
-    'arrowParens': 'avoid',
-    'trailingComma': 'all',
-    'bracketSpacing': true,
-    'overrides': [
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    arrowParens: "avoid",
+    trailingComma: "all",
+    bracketSpacing: true,
+    overrides: [
         {
-            'files': ['*.json', '*.yaml', '*.yml'],
-            'options': {
-                'tabWidth': 2
-            }
-        }
-    ]
-}
+            files: ["*.json", "*.yaml", "*.yml"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
