d360bbbcb39cfa08734db5d30a5d75dfcc3cef6c
hyrious <hyrious@outlook.com>
2021-10-20 11:11:10 +0800

    refactor(web): remove type error (#1016)
    - refactor(desktop): remove type error

--- desktop/renderer-app/src/components/Tips/ErrorTips.tsx
+++ desktop/renderer-app/src/components/Tips/ErrorTips.tsx
@@ -3,7 +3,7 @@ import { ServerRequestError } from "../../utils/error/server-request-error";
 import { NODE_ENV } from "../../constants/process";
 import { i18n } from "../../utils/i18n";
 
-export const errorTips = (e: Error): void => {
+export const errorTips = (e: unknown): void => {
     if (NODE_ENV === "development") {
         console.error(e);
     }
@@ -11,6 +11,6 @@ export const errorTips = (e: Error): void => {
     if (e instanceof ServerRequestError) {
         void message.error(i18n.t(e.errorMessage));
     } else {
-        void message.error(e.message);
+        void message.error((e as Error).message);
     }
 };
