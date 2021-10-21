d16c5f39c87a56d1279f7beba45fdd969847c114
crimx <straybugs@gmail.com>
2021-10-21 20:56:46 +0800

    feat(web): add vf html5 courseware

--- packages/flat-components/src/containers/CloudStorageContainer/store.ts
+++ packages/flat-components/src/containers/CloudStorageContainer/store.ts
@@ -151,4 +151,7 @@ export abstract class CloudStorageStore {
 
     /** When a filename is changed to a meaningful new name */
     abstract onNewFileName(fileUUID: FileUUID, fileName: CloudStorageFileName): void;
+
+    /** Add Online HTML5 Courseware to Cloud Storage */
+    abstract addOnlineH5(fileName: string, fileURL: string): Promise<void>;
 }
