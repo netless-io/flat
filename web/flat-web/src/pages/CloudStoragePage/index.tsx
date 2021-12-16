735b2b2f685630e8c6bada2f134687d4d272e82c
BlackHole1 <158blackhole@gmail.com>
2021-12-16 17:18:28 +0800

    refactor(project): when enter pan page, if jwt token expires, jump to login page

--- web/flat-web/src/pages/CloudStoragePage/index.tsx
+++ web/flat-web/src/pages/CloudStoragePage/index.tsx
@@ -5,16 +5,36 @@ import { CloudStorageContainer } from "flat-components";
 import { PageStoreContext } from "../../components/StoreProvider";
 import { CloudStorageStore } from "./store";
 import { useTranslation } from "react-i18next";
+import { loginCheck } from "../../api-middleware/flatServer";
+import { ServerRequestError } from "../../utils/error/server-request-error";
+import { RequestErrorCode } from "../../constants/error-code";
+import { RouteNameType } from "../../route-config";
+import { useReplaceHistory } from "../../utils/routes";
 
 export interface CloudStoragePageProps {}
 
 export const CloudStoragePage = observer<CloudStoragePageProps>(function CloudStoragePage() {
     const { i18n } = useTranslation();
+    const replaceHistory = useReplaceHistory();
     const [store] = useState(
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         () => new CloudStorageStore({ compact: false, insertCourseware: () => {}, i18n }),
     );
     useEffect(() => store.initialize(), [store]);
+
+    useEffect(() => {
+        loginCheck().catch(error => {
+            if (error instanceof ServerRequestError) {
+                if (
+                    [RequestErrorCode.JWTSignFailed, RequestErrorCode.NeedLoginAgain].includes(
+                        error.errorCode,
+                    )
+                ) {
+                    replaceHistory(RouteNameType.LoginPage);
+                }
+            }
+        });
+    }, [replaceHistory]);
 
     const pageStore = useContext(PageStoreContext);
 
