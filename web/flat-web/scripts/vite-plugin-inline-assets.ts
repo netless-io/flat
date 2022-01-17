d6f1b3e8a17f5ece0963b5afbffbd58281b1eb65
matrixbirds <yooobuntu@163.com>
2022-01-17 11:46:32 +0800

    fix(scripts): fix missing function

--- web/flat-web/scripts/vite-plugin-inline-assets.ts
+++ web/flat-web/scripts/vite-plugin-inline-assets.ts
@@ -5,7 +5,7 @@ import svgToTinyDataUri from "@netless/mini-svg-data-uri";
 
 // e.g:
 // flat/node_modules/electron/index.js?v=19cea64f => flat/node_modules/electron/index.js
-const removeQueryString = (url: string): string => url.replace(/#.*$/, "").replace(/\?.*$/, "");
+const cleanUrl = (url: string): string => url.replace(/#.*$/, "").replace(/\?.*$/, "");
 
 export function inlineAssets(): Plugin {
     return {
@@ -18,8 +18,8 @@ export function inlineAssets(): Plugin {
                 const url = filePath.endsWith(".svg")
                     ? svgToTinyDataUri(imageFileContent.toString("utf-8"), "utf8")
                     : `data:${mime.getType(filePath)};base64,${imageFileContent.toString(
-                        "base64",
-                    )}`;
+                          "base64",
+                      )}`;
                 return `const content = "${url}";export default content;`;
             }
             return null;
