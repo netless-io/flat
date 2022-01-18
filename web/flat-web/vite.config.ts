88afa06bd62422293df370a1029b7d6ac8c11e8a
舜岳 <tsunyue1320@gmail.com>
2022-01-18 13:00:40 +0800

    chore(web): improve vite config path (#1310)

--- web/flat-web/vite.config.ts
+++ web/flat-web/vite.config.ts
@@ -1,29 +1,42 @@
 import legacy from "@vitejs/plugin-legacy";
 import refresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
-import path from "path";
 import { dotenv } from "./scripts/vite-plugin-dotenv";
 import { injectHtmlHash } from "./scripts/vite-plugin-html-hash";
 import { version } from "./scripts/vite-plugin-version";
 import { inlineAssets } from "./scripts/vite-plugin-inline-assets";
+import {
+    configPath,
+    typesEntryPath,
+    i18nEntryPath,
+    componentsEntryPath,
+    mainPackageJSONPath,
+} from "../../scripts/constants";
 
 export default defineConfig({
-    plugins: [refresh(), legacy(), dotenv("../../config"), injectHtmlHash(), version(path.join(__dirname, "..", "..", "desktop", "main-app", "package.json")), inlineAssets()],
+    plugins: [
+        refresh(),
+        legacy(),
+        dotenv(configPath),
+        injectHtmlHash(),
+        version(mainPackageJSONPath),
+        inlineAssets(),
+    ],
     resolve: {
         alias: [
             // replace webpack alias
             { find: /^~/, replacement: "" },
             {
                 find: "flat-types",
-                replacement: path.join(__dirname, "..", "..", "packages", "flat-types", "src"),
+                replacement: typesEntryPath,
             },
             {
                 find: "flat-i18n",
-                replacement: path.join(__dirname, "..", "..", "packages", "flat-i18n", "locales"),
+                replacement: i18nEntryPath,
             },
             {
                 find: "flat-components",
-                replacement: path.join(__dirname, "..", "..", "packages", "flat-components", "src"),
+                replacement: componentsEntryPath,
             },
         ],
     },
