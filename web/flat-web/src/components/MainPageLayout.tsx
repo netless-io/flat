efd4a1cdf53478efc3f275412503697686f68575
CRIMX <straybugs@gmail.com>
2022-01-20 14:15:48 +0800

    refactor(web): move flat theme provider to AppRoutes

--- web/flat-web/src/components/MainPageLayout.tsx
+++ web/flat-web/src/components/MainPageLayout.tsx
@@ -1,29 +1,23 @@
 import React, { useContext } from "react";
 import { observer } from "mobx-react-lite";
-import { FlatThemeProvider } from "flat-components";
 import { MainPageLayoutHorizontalContainer } from "./MainPageLayoutHorizontalContainer";
 import { routePages } from "../AppRoutes/route-pages";
-import { ConfigStoreContext, PageStoreContext } from "./StoreProvider";
+import { PageStoreContext } from "./StoreProvider";
 
 export const MainPageLayout = observer(function MainPageLayout({ children }) {
     const pageStore = useContext(PageStoreContext);
-    const configStore = useContext(ConfigStoreContext);
     const hasHeader = pageStore.name && routePages[pageStore.name].hasHeader;
-    return (
-        <FlatThemeProvider prefersColorScheme={configStore.prefersColorScheme}>
-            {hasHeader ? (
-                <MainPageLayoutHorizontalContainer
-                    activeKeys={pageStore.activeKeys}
-                    subMenu={pageStore.subMenu}
-                    title={pageStore.title}
-                    onBackPreviousPage={pageStore.onBackPreviousPage}
-                    onRouteChange={pageStore.onRouteChange}
-                >
-                    {children}
-                </MainPageLayoutHorizontalContainer>
-            ) : (
-                <>{children}</>
-            )}
-        </FlatThemeProvider>
+    return hasHeader ? (
+        <MainPageLayoutHorizontalContainer
+            activeKeys={pageStore.activeKeys}
+            subMenu={pageStore.subMenu}
+            title={pageStore.title}
+            onBackPreviousPage={pageStore.onBackPreviousPage}
+            onRouteChange={pageStore.onRouteChange}
+        >
+            {children}
+        </MainPageLayoutHorizontalContainer>
+    ) : (
+        <>{children}</>
     );
 });
