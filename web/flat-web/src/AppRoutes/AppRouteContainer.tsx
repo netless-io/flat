4f5ec21f7f688023a5d4c03423b986cf156f47ba
CRIMX <straybugs@gmail.com>
2022-01-20 17:18:26 +0800

    refactor(web): move theme provider to app route container

--- web/flat-web/src/AppRoutes/AppRouteContainer.tsx
+++ web/flat-web/src/AppRoutes/AppRouteContainer.tsx
@@ -1,13 +1,14 @@
 import loadable from "@loadable/component";
-import React, { ComponentType, FC, useContext, useEffect } from "react";
+import React, { ComponentType, useContext, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { RouteComponentProps } from "react-router-dom";
 import { useIsomorphicLayoutEffect } from "react-use";
-import { LoadingPage } from "flat-components";
-import { PageStoreContext } from "../components/StoreProvider";
+import { FlatThemeBodyProvider, LoadingPage } from "flat-components";
+import { ConfigStoreContext, PageStoreContext } from "../components/StoreProvider";
 import { RouteNameType } from "../route-config";
 import { AppRouteErrorBoundary } from "./AppRouteErrorBoundary";
 import { routePages } from "./route-pages";
+import { observer } from "mobx-react-lite";
 
 export interface AppRouteContainerProps {
     name: RouteNameType;
@@ -16,13 +17,14 @@ export interface AppRouteContainerProps {
     routeProps: RouteComponentProps;
 }
 
-export const AppRouteContainer: FC<AppRouteContainerProps> = ({
+export const AppRouteContainer = observer<AppRouteContainerProps>(function AppRouteContainer({
     name,
     Comp,
     title,
     routeProps,
-}) => {
+}) {
     const pageStore = useContext(PageStoreContext);
+    const configStore = useContext(ConfigStoreContext);
     const { t } = useTranslation();
 
     useIsomorphicLayoutEffect(() => {
@@ -39,11 +41,13 @@ export const AppRouteContainer: FC<AppRouteContainerProps> = ({
     const hasHeader = pageStore.name && routePages[pageStore.name].hasHeader;
 
     return (
-        <AppRouteErrorBoundary
-            Comp={loadable(Comp, {
-                fallback: <LoadingPage hasHeader={hasHeader} />,
-            })}
-            {...{ title, routeProps }}
-        />
+        <FlatThemeBodyProvider prefersColorScheme={configStore.prefersColorScheme}>
+            <AppRouteErrorBoundary
+                Comp={loadable(Comp, {
+                    fallback: <LoadingPage hasHeader={hasHeader} />,
+                })}
+                {...{ title, routeProps }}
+            />
+        </FlatThemeBodyProvider>
     );
-};
+});
