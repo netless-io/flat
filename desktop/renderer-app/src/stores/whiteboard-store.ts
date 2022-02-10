765fff42364998de7c898c689ea517a6a7bc1382
cheerchen <meetcheerego@gmail.com>
2022-02-10 11:58:03 +0800

    feat(whiteboard): use the fastboard

--- desktop/renderer-app/src/stores/whiteboard-store.ts
+++ desktop/renderer-app/src/stores/whiteboard-store.ts
@@ -1,5 +1,6 @@
 import "video.js/dist/video-js.css";
 
+import { FastboardApp, createFastboard } from "@netless/fastboard";
 import type { Attributes as SlideAttributes } from "@netless/app-slide";
 import { AddAppParams, BuiltinApps, WindowManager } from "@netless/window-manager";
 import { message } from "antd";
@@ -8,16 +9,7 @@ import { v4 as v4uuid } from "uuid";
 import { debounce } from "lodash-es";
 import { makeAutoObservable, observable, runInAction } from "mobx";
 import { isMobile, isWindows } from "react-device-detect";
-import {
-    DefaultHotKeys,
-    DeviceType,
-    Room,
-    RoomPhase,
-    RoomState,
-    SceneDefinition,
-    ViewMode,
-    WhiteWebSdk,
-} from "white-web-sdk";
+import { DeviceType, Room, RoomPhase, RoomState, SceneDefinition, ViewMode } from "white-web-sdk";
 import { RoomType } from "../../../../packages/flat-components/src/types/room";
 import { CLOUD_STORAGE_DOMAIN, NETLESS, NODE_ENV } from "../constants/process";
 import { CloudStorageFile, CloudStorageStore } from "../pages/CloudStoragePage/store";
@@ -30,6 +22,7 @@ import { ServerRequestError } from "../utils/error/server-request-error";
 import { RequestErrorCode } from "../constants/error-code";
 
 export class WhiteboardStore {
+    public fastboardApp: FastboardApp | null = null;
     public room: Room | null = null;
     public phase: RoomPhase = RoomPhase.Connecting;
     public viewMode: ViewMode | null = null;
@@ -45,8 +38,6 @@ export class WhiteboardStore {
     public smallClassRatio = 8.3 / 16;
     public otherClassRatio = 10.46 / 16;
     public smallClassAvatarWrapMaxWidth = 0;
-    public redoSteps = 0;
-    public undoSteps = 0;
 
     /** is room Creator */
     public readonly isCreator: boolean;
@@ -64,28 +55,33 @@ export class WhiteboardStore {
     }) {
         this.isCreator = config.isCreator;
         this.isWritable = config.isCreator;
-        this.getRoomType = config.getRoomType;
         this.i18n = config.i18n;
+        this.getRoomType = config.getRoomType;
         this.onDrop = config.onDrop;
 
         makeAutoObservable<this, "preloadPPTResource">(this, {
             room: observable.ref,
             preloadPPTResource: false,
-            addMainViewScene: false,
-            nextMainViewScene: false,
-            preMainViewScene: false,
-            windowManager: false,
+            fastboardApp: false,
         });
 
         this.cloudStorageStore = new CloudStorageStore({
             compact: true,
-            insertCourseware: this.insertCourseware,
             i18n: this.i18n,
+            insertCourseware: this.insertCourseware,
         });
     }
 
+    public updateFastboardApp = (whiteboardApp: FastboardApp): void => {
+        this.fastboardApp = whiteboardApp;
+    };
+
     public updateRoom = (room: Room): void => {
         this.room = room;
+    };
+
+    public updateWindowManager = (manager: WindowManager): void => {
+        this.windowManager = manager;
     };
 
     public updatePhase = (phase: RoomPhase): void => {
@@ -110,16 +106,6 @@ export class WhiteboardStore {
         }
     };
 
-    public updateWindowManager = (windowManager: WindowManager): void => {
-        if (process.env.DEV) {
-            (window as any).manager = windowManager;
-        }
-        this.windowManager = windowManager;
-
-        this.updateCurrentSceneIndex(windowManager.mainViewSceneIndex);
-        this.windowManager = windowManager;
-    };
-
     public updateCurrentSceneIndex = (currentSceneIndex: number): void => {
         this.currentSceneIndex = currentSceneIndex;
     };
@@ -130,14 +116,6 @@ export class WhiteboardStore {
 
     public updateWindowMaximization = (isMaximization: boolean): void => {
         this.isWindowMaximization = isMaximization;
-    };
-
-    public updateRedoSteps = (steps: number): void => {
-        this.redoSteps = steps;
-    };
-
-    public updateUndoSteps = (steps: number): void => {
-        this.undoSteps = steps;
     };
 
     public updateSmallClassAvatarWrapMaxWidth = (smallClassAvatarWrapMaxWidth: number): void => {
@@ -255,43 +233,6 @@ export class WhiteboardStore {
         await this.windowManager?.addApp(config);
     };
 
-    public onWindowManagerBoxStateChange = (
-        initialBoxState?: "normal" | "minimized" | "maximized",
-    ): void => {
-        this.updateWindowMaximization(Boolean(initialBoxState === "maximized"));
-
-        this.windowManager?.emitter.on("boxStateChange", mode => {
-            const isMaximization = mode === "maximized";
-            this.updateWindowMaximization(isMaximization);
-        });
-    };
-
-    public onMainViewSceneChange = (): void => {
-        this.windowManager?.emitter.on("mainViewSceneIndexChange", scene => {
-            if (this.room) {
-                this.updateCurrentSceneIndex(scene);
-            }
-        });
-    };
-
-    public onMainViewSceneLengthChange = (): void => {
-        this.windowManager?.emitter.on("mainViewScenesLengthChange", length => {
-            this.updateScenesCount(length);
-        });
-    };
-
-    public onMainViewRedoUndoStepsChange = (): void => {
-        this.windowManager?.mainView.callbacks.on("onCanUndoStepsUpdate", this.updateUndoSteps);
-        this.windowManager?.mainView.callbacks.on("onCanRedoStepsUpdate", this.updateRedoSteps);
-    };
-
-    public destroyWindowManager = (): void => {
-        this.windowManager?.mainView.callbacks.off("onCanUndoStepsUpdate", this.updateUndoSteps);
-        this.windowManager?.mainView.callbacks.off("onCanRedoStepsUpdate", this.updateRedoSteps);
-        this.windowManager?.destroy();
-        this.windowManager = null;
-    };
-
     public async joinWhiteboardRoom(): Promise<void> {
         if (!globalStore.userUUID) {
             throw new Error("Missing userUUID");
@@ -311,25 +252,33 @@ export class WhiteboardStore {
                 deviceType = DeviceType.Desktop;
             }
         }
-        const whiteWebSdk = new WhiteWebSdk({
-            appIdentifier: NETLESS.APP_IDENTIFIER,
-            deviceType: deviceType,
-            pptParams: {
-                useServerWrap: true,
-            },
-            useMobXState: true,
-        });
 
         const cursorName = globalStore.userInfo?.name;
 
-        const room = await whiteWebSdk.joinRoom(
-            {
+        const fastboardApp = await createFastboard({
+            sdkConfig: {
+                appIdentifier: NETLESS.APP_IDENTIFIER,
+                deviceType: deviceType,
+                pptParams: {
+                    useServerWrap: true,
+                },
+            },
+            managerConfig: {
+                cursor: true,
+                chessboard: false,
+                containerSizeRatio: this.getWhiteboardRatio(),
+                collectorStyles: {
+                    position: "absolute",
+                    bottom: "8px",
+                },
+            },
+            joinRoom: {
                 uuid: globalStore.whiteboardRoomUUID,
                 roomToken: globalStore.whiteboardRoomToken,
                 region: globalStore.region ?? undefined,
                 userPayload: {
                     uid: globalStore.userUUID,
-                    nickName: cursorName,
+                    nickName: globalStore.userInfo?.name,
                     // @deprecated
                     userId: globalStore.userUUID,
                     // @deprecated
@@ -337,36 +286,15 @@ export class WhiteboardStore {
                 },
                 floatBar: true,
                 isWritable: this.isWritable,
-                disableNewPencil: false,
-                hotKeys: {
-                    ...DefaultHotKeys,
-                    changeToSelector: "s",
-                    changeToLaserPointer: "z",
-                    changeToPencil: "p",
-                    changeToRectangle: "r",
-                    changeToEllipse: "c",
-                    changeToEraser: "e",
-                    changeToText: "t",
-                    changeToStraight: "l",
-                    changeToArrow: "a",
-                    changeToHand: "h",
-                },
-                useMultiViews: true,
                 invisiblePlugins: [WindowManager],
                 uid: globalStore.userUUID,
-                disableMagixEventDispatchLimit: true,
-            },
-            {
-                onPhaseChanged: phase => {
-                    this.updatePhase(phase);
-                },
-                onRoomStateChanged: async (modifyState: Partial<RoomState>): Promise<void> => {
-                    if (modifyState.broadcastState) {
-                        this.updateViewMode(modifyState.broadcastState.mode);
-                    }
+                callbacks: {
+                    onPhaseChanged: phase => this.updatePhase(phase),
+                    onRoomStateChanged: async (modifyState: Partial<RoomState>): Promise<void> => {
+                        if (modifyState.broadcastState) {
+                            this.updateViewMode(modifyState.broadcastState.mode);
+                        }
 
-                    // TODO: That is temporarily plan, use RTM emit message is follow-up plan.
-                    try {
                         const pptSrc = modifyState.sceneState?.scenes[0]?.ppt?.src;
                         if (pptSrc) {
                             try {
@@ -375,35 +303,41 @@ export class WhiteboardStore {
                                 console.log(err);
                             }
                         }
-                    } catch (err) {
-                        console.log(err);
-                    }
-                },
-                onDisconnectWithError: error => {
-                    void message.error(this.i18n.t("on-disconnect-with-error"));
-                    this.preloadPPTResource.cancel();
-                    console.error(error);
-                },
-                onKickedWithReason: reason => {
-                    if (
-                        reason === "kickByAdmin" ||
-                        reason === "roomDelete" ||
-                        reason === "roomBan"
-                    ) {
-                        // Kick in-room joiners when creator cancels room
-                        // from the homepage list menu
-                        runInAction(() => {
-                            // Room creator do not need to listen to this event
-                            // as they are in control of exiting room.
-                            // Listening to this may interrupt the stop room process.
-                            if (!this.isCreator) {
-                                this.isKicked = true;
-                            }
-                        });
-                    }
+                    },
+                    onDisconnectWithError: error => {
+                        void message.error(this.i18n.t("on-disconnect-with-error"));
+                        console.error(error);
+                        this.preloadPPTResource.cancel();
+                    },
+                    onKickedWithReason: reason => {
+                        if (
+                            reason === "kickByAdmin" ||
+                            reason === "roomDelete" ||
+                            reason === "roomBan"
+                        ) {
+                            // Kick in-room joiners when creator cancels room
+                            // from the homepage list menu
+                            runInAction(() => {
+                                // Room creator do not need to listen to this event
+                                // as they are in control of exiting room.
+                                // Listening to this may interrupt the stop room process.
+                                if (!this.isCreator) {
+                                    this.isKicked = true;
+                                }
+                            });
+                        }
+                    },
                 },
             },
-        );
+        });
+
+        this.updateFastboardApp(fastboardApp);
+
+        const { room, manager } = fastboardApp;
+
+        this.updateRoom(room);
+
+        this.updateWindowManager(manager);
 
         room.disableDeviceInputs = !this.isWritable;
 
@@ -411,20 +345,15 @@ export class WhiteboardStore {
             this.updateViewMode(room.state.broadcastState.mode);
         }
 
-        this.updateRoom(room);
-
-        this.updateScenesCount(room.state.sceneState.scenes.length);
-
         if (NODE_ENV === "development") {
             (window as any).room = room;
             (window as any).manager = this.windowManager;
         }
     }
 
-    public destroy(): void {
+    public async destroy(): Promise<void> {
         this.preloadPPTResource.cancel();
-        this.destroyWindowManager();
-        this.room?.callbacks.off();
+        await this.fastboardApp?.destroy();
 
         if (NODE_ENV === "development") {
             (window as any).room = null;
