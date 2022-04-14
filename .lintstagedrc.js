a83ad8bd403d68458f63035a5624ee9e1efaa9fe
Black-Hole <158blackhole@gmail.com>
2022-04-14 11:51:31 +0800

    fix(lint): git commit failed after change pnpm-lock.yaml file (#1475)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
     "*.svg": "svgo",
-    "*.{md,ts,tsx,js,css,less,json,yml,yaml,html,sh}": "cspell --no-progress",
+    "*.{md,ts,tsx,js,css,less,json,yml,yaml,html,sh}": "cspell --no-progress --no-must-find-files",
 };
