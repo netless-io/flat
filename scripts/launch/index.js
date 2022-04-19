dc69d3d97b7c5255c0a96e3556aecb6d76c693c8
hyrious <hyrious@outlook.com>
2022-04-19 17:57:16 +0800

    chore(project): fix electron start script with region

--- scripts/launch/index.js
+++ scripts/launch/index.js
@@ -11,16 +11,19 @@ function parseStdout({ stdout }) {
     return stdout.toString().trim();
 }
 
+const { FLAT_REGION } = process.env;
+const suffix = FLAT_REGION ? ":" + FLAT_REGION.toLowerCase() : "";
+
 if (process.platform === "win32") {
-    system("scripts\\launch\\start.cmd");
+    system("scripts\\launch\\start.cmd " + suffix);
 } else if (process.platform === "darwin") {
     const existsITerm2 = parseStdout(
         system("osascript ./scripts/launch/mac/exists_iTerm.scpt", false),
     );
 
     if (existsITerm2 === "true") {
-        system("osascript ./scripts/launch/mac/launch_iTerm.scpt");
+        system("osascript ./scripts/launch/mac/launch_iTerm.scpt " + suffix);
     } else {
-        system("osascript ./scripts/launch/mac/launch_terminal.scpt");
+        system("osascript ./scripts/launch/mac/launch_terminal.scpt " + suffix);
     }
 }
