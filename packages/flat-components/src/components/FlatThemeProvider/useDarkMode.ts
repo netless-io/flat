fd9dd1514dc117e1c78512495acdcecec0b584d5
hyrious <hyrious@outlook.com>
2022-05-26 15:32:12 +0800

    fix(component): visual bug in auto dark mode (#1531)

--- packages/flat-components/src/components/FlatThemeProvider/useDarkMode.ts
+++ packages/flat-components/src/components/FlatThemeProvider/useDarkMode.ts
@@ -3,19 +3,21 @@ import { useIsomorphicLayoutEffect } from "react-use";
 
 export type FlatPrefersColorScheme = "auto" | "dark" | "light";
 
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+
 export function useDarkMode(prefersColorScheme?: FlatPrefersColorScheme): boolean {
-    const [darkMode, setDarkMode] = useState(prefersColorScheme === "dark");
+    const [darkMode, setDarkMode] = useState(() =>
+        prefersColorScheme === "auto" && prefersDark
+            ? prefersDark.matches
+            : prefersColorScheme === "dark",
+    );
 
     useIsomorphicLayoutEffect(() => {
-        if (prefersColorScheme === "auto") {
-            const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
-            if (prefersDark) {
-                setDarkMode(prefersDark.matches);
-                const handler = (evt: MediaQueryListEvent): void => setDarkMode(evt.matches);
-                prefersDark.addListener(handler);
-                return () => prefersDark.removeListener(handler);
-            }
-            return;
+        if (prefersColorScheme === "auto" && prefersDark) {
+            setDarkMode(prefersDark.matches);
+            const handler = (evt: MediaQueryListEvent): void => setDarkMode(evt.matches);
+            prefersDark.addEventListener("change", handler);
+            return () => prefersDark.removeEventListener("change", handler);
         } else {
             setDarkMode(prefersColorScheme === "dark");
             return;
