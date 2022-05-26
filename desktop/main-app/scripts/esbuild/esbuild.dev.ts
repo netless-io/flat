14e360b4e094c8ad1dd2dfdb59482daf9920d9e1
Cyberhan123 <255542417@qq.com>
2022-05-26 23:21:37 +0800

    feat(main-app): change main app to esbuild
    Co-authored-by: hyrious <hyrious@outlook.com>

---
+++ desktop/main-app/scripts/esbuild/esbuild.dev.ts
@@ -0,0 +1,52 @@
+import { ChildProcess, spawn } from "child_process";
+import esbuild from "esbuild";
+import { dotenvPlugin, external } from "./esbuild.common";
+import * as paths from "./paths";
+
+let child: ChildProcess | undefined;
+const respawn = () => {
+    if (child) {
+        child.kill("SIGTERM");
+    }
+    child = spawn("pnpm", ["electron", paths.dist], { stdio: "inherit" });
+};
+
+const buildPreload = esbuild.build({
+    entryPoints: [paths.preloadPath],
+    bundle: true,
+    platform: "browser",
+    target: "chrome89",
+    external: [...external, "electron", "os", "path"],
+    outfile: paths.preloadDist,
+    watch: true,
+});
+
+const buildMain = esbuild.build({
+    entryPoints: [paths.entryFile],
+    bundle: true,
+    platform: "node",
+    target: "node14",
+    external: [...external, "electron", "electron-devtools-vendor"],
+    sourcemap: true,
+    outfile: paths.dist,
+    watch: {
+        onRebuild(error) {
+            if (error) {
+                console.error("watch build failed:", error);
+            } else {
+                respawn();
+            }
+        },
+    },
+    plugins: [dotenvPlugin],
+});
+
+const allDone = Promise.all([buildPreload, buildMain]);
+
+const stop = () =>
+    allDone.then(rs => rs.forEach(r => r.stop && r.stop())).catch(() => process.exit(1));
+
+process.on("SIGINT", stop);
+process.on("SIGTERM", stop);
+
+allDone.then(respawn);
---
+++ desktop/main-app/scripts/esbuild/plugin/dotEnvFlowPlugin.ts
@@ -0,0 +1,58 @@
+import { Plugin, PluginBuild } from "esbuild";
+import dotenvFlow, { DotenvConfigOptions } from "dotenv-flow";
+
+function parseWithEnvObject(orgEnv: Record<string, string | undefined>): Record<string, string> {
+    return Reflect.ownKeys(orgEnv)
+        ?.map(item => {
+            const result: Record<string, any> = {};
+            if (typeof item === "string" && orgEnv?.[item]) {
+                result[`process.env.${item}`] = JSON.stringify(orgEnv?.[item]);
+            }
+            return result;
+        })
+        ?.reduce((prev, cur) => {
+            return {
+                ...prev,
+                ...cur,
+            };
+        }, {});
+}
+
+// Configuration define refer to https://github.com/kerimdzhanov/dotenv-flow-webpack
+type DotEnvPluginOptionsType = Omit<DotenvConfigOptions, "purge_dotenv"> & {
+    system_vars?: boolean;
+};
+
+type DotEnvPluginType = (options: DotEnvPluginOptionsType) => Plugin;
+
+const dotEnvFlowPlugin: DotEnvPluginType = options => {
+    return {
+        name: "DotEnvFlow",
+        setup(build: PluginBuild) {
+            let sysEnvList = {};
+
+            const esbuildOptions = build.initialOptions;
+
+            const result = dotenvFlow.config(options);
+
+            if (result.error) {
+                console.warn("esbuild dotenvFlow throw error:", result.error);
+                return;
+            }
+
+            if (options?.system_vars) {
+                sysEnvList = parseWithEnvObject(process.env);
+            }
+
+            const configEnvList = parseWithEnvObject(result?.parsed || {});
+
+            esbuildOptions.define = {
+                ...esbuildOptions?.define,
+                ...sysEnvList,
+                ...configEnvList,
+            };
+        },
+    };
+};
+
+export default dotEnvFlowPlugin;
