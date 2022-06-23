1eeb65224e18dcfd9d16aab7367525f925320720
舜岳 <tsunyue1320@gmail.com>
2022-06-23 17:16:29 +0800

    fix(flat-components): loginTitle stories props `LoginPanelProps` to `LoginTitleProps` (#1584)

--- packages/flat-components/src/components/LoginPage/LoginTitle/LoginTitle.stories.tsx
+++ packages/flat-components/src/components/LoginPage/LoginTitle/LoginTitle.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, Story } from "@storybook/react";
 import React from "react";
-import { LoginTitle } from ".";
-import { LoginPanelProps } from "..";
+import { LoginTitle, LoginTitleProps } from ".";
 
 const storyMeta: Meta = {
     title: "LoginPage/LoginTitle",
@@ -10,4 +9,4 @@ const storyMeta: Meta = {
 
 export default storyMeta;
 
-export const Overview: Story<LoginPanelProps> = props => <LoginTitle {...props} />;
+export const Overview: Story<LoginTitleProps> = props => <LoginTitle {...props} />;
