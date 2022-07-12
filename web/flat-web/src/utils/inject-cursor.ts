df4312c768a0c6e79b56283c2cd8045e2d869b44
hyrious <hyrious@outlook.com>
2022-07-12 15:36:10 +0800

    refactor: also inject cursor for shapes

--- web/flat-web/src/utils/inject-cursor.ts
+++ web/flat-web/src/utils/inject-cursor.ts
@@ -10,16 +10,23 @@ type CursorName = `${CursorNames}`;
  * $el.style.cursor = getCircle('fff')
  * ```
  */
-function getCursorUrl(color: Color): string {
+function getCircleUrl(color: Color): string {
     // eslint-disable-next-line @typescript-eslint/quotes
     return `url("data:image/svg+xml,%3Csvg width='24' height='24' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Ccircle cx='12' cy='12' r='2.5' stroke='%23${color}' stroke-linejoin='square'/%3E%3Ccircle cx='12' cy='12' r='3.5' stroke='%23${color}'/%3E%3C/g%3E%3C/svg%3E") 12 12, auto;`; // cspell:disable-line
+}
+
+function getCrossUrl(color: Color): string {
+    // eslint-disable-next-line @typescript-eslint/quotes
+    return `url("data:image/svg+xml,%3Csvg width='24' height='24' xmlns='http://www.w3.org/2000/svg' fill='none'%3E%3Cpath d='M5 12H19' stroke='%23${color}' stroke-linejoin='round'/%3E%3Cpath d='M12 5V19' stroke='%23${color}' stroke-linejoin='round'/%3E%3C/svg%3E%0A") 12 12, auto`; // cspell:disable-line
 }
 
 function makeStyleContent(config: Partial<Record<CursorName, Color>>): string {
     let result = "";
     Object.keys(config).forEach(cursorName => {
-        const color = config[cursorName as CursorName] as Color;
-        result += `.netless-whiteboard.${cursorName} {cursor: ${getCursorUrl(color)}}\n`;
+        const cursor = cursorName as CursorName;
+        const color = config[cursor] as Color;
+        const getter = cursor === "cursor-pencil" ? getCircleUrl : getCrossUrl;
+        result += `.netless-whiteboard.${cursor} {cursor: ${getter(color)}}\n`;
     });
     return result;
 }
@@ -53,7 +60,14 @@ export function injectCursor(app: FastboardApp): () => void {
     const dispose = app.memberState.subscribe(state => {
         const [r, g, b] = state.strokeColor;
         const hex = ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-        cursorInjector.enable({ "cursor-pencil": hex });
+        cursorInjector.enable({
+            "cursor-pencil": hex,
+            "cursor-rectangle": hex,
+            "cursor-ellipse": hex,
+            "cursor-straight": hex,
+            "cursor-arrow": hex,
+            "cursor-shape": hex,
+        });
     });
     return () => {
         cursorInjector.disable();
