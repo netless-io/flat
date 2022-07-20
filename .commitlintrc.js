8721fc1b6d275812e9e76b4ed500eac6357bfe03
CRIMX <straybugs@gmail.com>
2022-07-20 14:54:39 +0800

    refactor(flat-pages): move big classroom to flat pages

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -66,6 +66,7 @@ module.exports = {
                 "flat-server-api",
                 "flat-stores",
                 "flat-services",
+                "flat-pages",
                 "pnpm",
                 "server",
             ],
