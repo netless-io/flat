b6202239f2dcc82606dfa46ad00d6cd29207ead9
cheerchen <meetcheerego@gmail.com>
2022-08-03 15:36:37 +0800

    refactor(renderer-app): update flat-services videoChat in the SystemCheckPage

--- desktop/renderer-app/src/pages/DeviceCheckPages/SystemCheckPage/index.tsx
+++ desktop/renderer-app/src/pages/DeviceCheckPages/SystemCheckPage/index.tsx
@@ -1,5 +1,5 @@
 import "./index.less";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "antd";
 import { useHistory, useLocation } from "react-router-dom";
 import os from "os";
@@ -8,6 +8,7 @@ import { DeviceCheckLayoutContainer } from "../DeviceCheckLayoutContainer";
 import { DeviceCheckResults, DeviceCheckState } from "../utils";
 import { useTranslation } from "react-i18next";
 import { IServiceVideoChatNetworkQualityType } from "@netless/flat-services";
+import { withFlatServices } from "@netless/flat-pages/src/components/FlatServicesContext";
 
 const networkDescription = [
     "network-quality-unknown",
@@ -21,78 +22,81 @@ const networkDescription = [
     "testing",
 ];
 
-export const SystemCheckPage = (): React.ReactElement => {
-    const { t } = useTranslation();
-    const [networkSituation, setNetworkSituation] =
-        useState<IServiceVideoChatNetworkQualityType>(8);
-    const rtc = useContext(FlatRTCContext);
-    const resultRef = React.useRef<DeviceCheckState>();
-    const history = useHistory<DeviceCheckResults>();
-    const location = useLocation<DeviceCheckResults | undefined>();
+export const SystemCheckPage = withFlatServices("videoChat")(
+    ({ videoChat: rtc }): React.ReactElement => {
+        const { t } = useTranslation();
+        const [networkSituation, setNetworkSituation] =
+            useState<IServiceVideoChatNetworkQualityType>(8);
+        const resultRef = React.useRef<DeviceCheckState>();
+        const history = useHistory<DeviceCheckResults>();
+        const location = useLocation<DeviceCheckResults | undefined>();
 
-    const cpuModel = os.cpus()[0].model;
-    const freeMemory = (os.freemem() / 1024 / 1024).toFixed(2);
+        const cpuModel = os.cpus()[0].model;
+        const freeMemory = (os.freemem() / 1024 / 1024).toFixed(2);
 
-    useEffect(() => {
-        const onError = (e: Error): void => {
-            console.error("rtc error", e);
-            resultRef.current = { content: t("testing-failed"), hasError: true };
-        };
-
-        rtc.events.on("error", onError);
-        rtc.events.on("network-test", setNetworkSituation);
-
-        rtc.startNetworkTest();
-
-        return () => {
-            rtc.events.off("error", onError);
-            rtc.events.off("network-test", setNetworkSituation);
-            rtc.stopNetworkTest();
-        };
-    }, [rtc, t]);
-
-    useEffect(() => {
-        if (networkSituation >= 5 && networkSituation <= 7) {
-            resultRef.current = {
-                content: t(`network-status.${networkDescription[networkSituation]}`),
-                hasError: true,
+        useEffect(() => {
+            const onError = (e: Error): void => {
+                console.error("rtc error", e);
+                resultRef.current = { content: t("testing-failed"), hasError: true };
             };
-        } else {
-            resultRef.current = { content: "", hasError: false };
+
+            rtc.events.on("error", onError);
+            rtc.events.on("network-test", setNetworkSituation);
+
+            rtc.startNetworkTest();
+
+            return () => {
+                rtc.events.off("error", onError);
+                rtc.events.off("network-test", setNetworkSituation);
+                rtc.stopNetworkTest();
+            };
+        }, [rtc, t]);
+
+        useEffect(() => {
+            if (networkSituation >= 5 && networkSituation <= 7) {
+                resultRef.current = {
+                    content: t(`network-status.${networkDescription[networkSituation]}`),
+                    hasError: true,
+                };
+            } else {
+                resultRef.current = { content: "", hasError: false };
+            }
+        }, [networkSituation, t]);
+
+        function historyPush(): void {
+            history.push({
+                pathname: routeConfig.CameraCheckPage.path,
+                state: {
+                    ...location.state,
+                    systemCheck: resultRef.current,
+                },
+            });
         }
-    }, [networkSituation, t]);
 
-    function historyPush(): void {
-        history.push({
-            pathname: routeConfig.CameraCheckPage.path,
-            state: {
-                ...location.state,
-                systemCheck: resultRef.current,
-            },
-        });
-    }
-
-    return (
-        <DeviceCheckLayoutContainer>
-            <div className="system-check-container">
-                <div className="system-check-item">
-                    <span className="system-check-item-name">{t("cpu")}</span>
-                    <span>{cpuModel}</span>
+        return (
+            <DeviceCheckLayoutContainer>
+                <div className="system-check-container">
+                    <div className="system-check-item">
+                        <span className="system-check-item-name">{t("cpu")}</span>
+                        <span>{cpuModel}</span>
+                    </div>
+                    <div className="system-check-item">
+                        <span className="system-check-item-name">
+                            {t("cache-available-storage-space")}
+                        </span>
+                        <span>{freeMemory} MB</span>
+                    </div>
+                    <div className="system-check-item">
+                        <span className="system-check-item-name">
+                            {t("network-quality-situation")}
+                        </span>
+                        <span>{t(`network-status.${networkDescription[networkSituation]}`)}</span>
+                    </div>
                 </div>
-                <div className="system-check-item">
-                    <span className="system-check-item-name">
-                        {t("cache-available-storage-space")}
-                    </span>
-                    <span>{freeMemory} MB</span>
-                </div>
-                <div className="system-check-item">
-                    <span className="system-check-item-name">{t("network-quality-situation")}</span>
-                    <span>{t(`network-status.${networkDescription[networkSituation]}`)}</span>
-                </div>
-            </div>
-            <Button type="primary" onClick={historyPush}>
-                {t("next-step")}
-            </Button>
-        </DeviceCheckLayoutContainer>
-    );
-};
+                <Button type="primary" onClick={historyPush}>
+                    {t("next-step")}
+                </Button>
+            </DeviceCheckLayoutContainer>
+        );
+    },
+);
