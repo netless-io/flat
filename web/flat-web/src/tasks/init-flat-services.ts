7cfbcb410c08e1e1b6e952b6e7e8b294797132b1
CRIMX <straybugs@gmail.com>
2022-08-04 11:19:39 +0800

    refactor(services): add service provider prefix

--- web/flat-web/src/tasks/init-flat-services.ts
+++ web/flat-web/src/tasks/init-flat-services.ts
@@ -2,12 +2,12 @@ import { flatServices } from "@netless/flat-services";
 
 export function initFlatServices(): void {
     flatServices.register("videoChat", async () => {
-        const { AgoraRTCWeb } = await import("@netless/agora-rtc-web");
+        const { AgoraRTCWeb } = await import("@netless/flat-service-provider-agora-rtc-web");
         return new AgoraRTCWeb({ APP_ID: process.env.AGORA_APP_ID });
     });
 
     flatServices.register("textChat", async () => {
-        const { AgoraRTM } = await import("@netless/agora-rtm");
+        const { AgoraRTM } = await import("@netless/flat-service-provider-agora-rtm");
         AgoraRTM.APP_ID = process.env.AGORA_APP_ID;
         return AgoraRTM.getInstance();
     });
