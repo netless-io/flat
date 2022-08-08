08c750326fb774d877353516b763d91cfaa2b06d
Cheerego7 <meetcheerego@gmail.com>
2022-08-08 11:11:57 +0800

    fix(renderer-app): use page store and disable MainPageLayout component in class page (#1633)
    * refactor(renderer-app): use page store and adjust style
    
    * fix(renderer-app): disable MainPageLayout component in class page

--- packages/flat-pages/src/utils/join-room-handler.ts
+++ packages/flat-pages/src/utils/join-room-handler.ts
@@ -1,4 +1,4 @@
-import { generateRoutePath, RouteNameType, usePushHistory } from "../utils/routes";
+import { RouteNameType, usePushHistory } from "../utils/routes";
 import { roomStore, globalStore } from "@netless/flat-stores";
 import { RoomType } from "@netless/flat-server-api";
 import { errorTips } from "flat-components";
@@ -14,22 +14,18 @@ export const joinRoomHandler = async (
         const data = await roomStore.joinRoom(periodicUUID || formatRoomUUID);
         globalStore.updateShowGuide(data.showGuide);
         globalStore.updatePeriodicUUID(roomInfo?.periodicUUID);
-        // try to work around chrome does not show permission popup after
-        // soft navigating. here we do a "hard" navigating instead.
+
         switch (data.roomType) {
             case RoomType.BigClass: {
-                // pushHistory(RouteNameType.BigClassPage, data);
-                window.location.href = generateRoutePath(RouteNameType.BigClassPage, data);
+                pushHistory(RouteNameType.BigClassPage, data);
                 break;
             }
             case RoomType.SmallClass: {
-                // pushHistory(RouteNameType.SmallClassPage, data);
-                window.location.href = generateRoutePath(RouteNameType.SmallClassPage, data);
+                pushHistory(RouteNameType.SmallClassPage, data);
                 break;
             }
             case RoomType.OneToOne: {
-                // pushHistory(RouteNameType.OneToOnePage, data);
-                window.location.href = generateRoutePath(RouteNameType.OneToOnePage, data);
+                pushHistory(RouteNameType.OneToOnePage, data);
                 break;
             }
             default: {
