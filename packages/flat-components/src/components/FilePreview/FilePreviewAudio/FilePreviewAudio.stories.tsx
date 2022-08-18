892129a751e6f25df66cd51b28f6d1cce467836d
Cheerego7 <meetcheerego@gmail.com>
2022-08-18 12:28:11 +0800

    style(flat-components): add height value (#1642)

--- packages/flat-components/src/components/FilePreview/FilePreviewAudio/FilePreviewAudio.stories.tsx
+++ packages/flat-components/src/components/FilePreview/FilePreviewAudio/FilePreviewAudio.stories.tsx
@@ -12,12 +12,14 @@ export default storyMeta;
 
 export const Overview: Story<FilePreviewAudioProps> = () => {
     return (
-        <FilePreviewAudio
-            file={randomCloudFile({
-                fileName: "test.mp3",
-                fileURL:
-                    "https://flat-storage.oss-accelerate.aliyuncs.com/cloud-storage/2022-03/28/f663cdcc-3367-4a15-8d2d-65fa4302c782/f663cdcc-3367-4a15-8d2d-65fa4302c782.mp3",
-            })}
-        />
+        <div className="vh-75">
+            <FilePreviewAudio
+                file={randomCloudFile({
+                    fileName: "test.mp3",
+                    fileURL:
+                        "https://flat-storage.oss-accelerate.aliyuncs.com/cloud-storage/2022-03/28/f663cdcc-3367-4a15-8d2d-65fa4302c782/f663cdcc-3367-4a15-8d2d-65fa4302c782.mp3",
+                })}
+            />
+        </div>
     );
 };
