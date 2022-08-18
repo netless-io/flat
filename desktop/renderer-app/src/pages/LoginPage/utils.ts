f7e3c92753745648182c53ef1eab007874a2c795
cheerchen <meetcheerego@gmail.com>
2022-08-18 16:20:39 +0800

    refactor(renderer-app): remove unused component of renderer-app

--- desktop/renderer-app/src/pages/LoginPage/utils.ts
+++
@@ -1,7 +0,0 @@
-import { LoginProcessResult } from "../../api-middleware/flatServer";
-
-export type LoginDisposer = () => void;
-
-export type LoginExecutor = (
-    onSuccess: (successResult: LoginProcessResult) => void,
-) => LoginDisposer;
