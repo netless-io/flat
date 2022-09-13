b6f59ff1c87945a54828732b38faaa95ab1d08d7
hyrious <hyrious@outlook.com>
2022-09-13 15:25:20 +0800

    Update desktop/main-app/scripts/esbuild/paths.ts
    Co-authored-by: Black-Hole <158blackhole@gmail.com>

--- desktop/main-app/scripts/esbuild/paths.ts
+++ desktop/main-app/scripts/esbuild/paths.ts
@@ -3,7 +3,7 @@ import path from "path";
 const resolvePath = (...relativePath: string[]): string =>
     path.resolve(__dirname, "..", "..", ...relativePath);
 
-export const dist = resolvePath("dist/index.js");
-export const preloadDist = resolvePath("dist/preload.js");
+export const dist = resolvePath("dist", "index.js");
+export const preloadDist = resolvePath("dist", "preload.js");
 export const entryFile = resolvePath("src", "index.ts");
 export const preloadPath = resolvePath("src", "preload.ts");
