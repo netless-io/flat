987111eb425ebdd32ab44da68f382c6f76620415
hyrious <hyrious@outlook.com>
2022-09-16 17:58:03 +0800

    refactor(flat-pages): add small class layout

--- packages/flat-pages/src/ReplayPage/index.tsx
+++ packages/flat-pages/src/ReplayPage/index.tsx
@@ -1,21 +1,21 @@
 import "video.js/dist/video-js.min.css";
 import "./ReplayPage.less";
 
+import classNames from "classnames";
 import { observer } from "mobx-react-lite";
-import React, { useEffect, useMemo, useRef } from "react";
+import React from "react";
 import { RouteComponentProps } from "react-router-dom";
 
 import { RoomType } from "@netless/flat-server-api";
 import { LoadingPage } from "flat-components";
 
+import ChatPanelReplay from "../components/ChatPanelReplay";
+import RealtimePanel from "../components/RealtimePanel";
 import { useClassroomReplayStore } from "../utils/use-classroom-replay-store";
 import { useLoginCheck } from "../utils/use-login-check";
 import { ReplayList } from "./ReplayList";
+import { ReplayVideo } from "./ReplayVideo";
 import { ReplayWhiteboard } from "./ReplayWhiteboard";
-import { ClassroomReplayStore, User } from "@netless/flat-stores";
-import RealtimePanel from "../components/RealtimePanel";
-import ChatPanelReplay from "../components/ChatPanelReplay";
-import classNames from "classnames";
 
 export type ReplayPageProps = RouteComponentProps<{
     roomUUID: string;
@@ -32,8 +32,27 @@ export const ReplayPage = observer<ReplayPageProps>(function ReplayPage({ match 
         return <LoadingPage />;
     }
 
+    const isSmallClass = classroomReplayStore.roomType === RoomType.SmallClass;
+
+    const ReplayVideos = (
+        <div className={classNames("replay-videos", { "is-horizontal": isSmallClass })}>
+            <ReplayVideo
+                classroomReplayStore={classroomReplayStore}
+                user={classroomReplayStore.users.creator}
+            />
+            {classroomReplayStore.onStageUserUUIDs.map(uuid => (
+                <ReplayVideo
+                    key={uuid}
+                    classroomReplayStore={classroomReplayStore}
+                    video={classroomReplayStore.userVideos.get(uuid)}
+                />
+            ))}
+        </div>
+    );
+
     return (
         <div className="replay-container">
+            {isSmallClass && ReplayVideos}
             <div className="replay-content">
                 <div className="replay-left">
                     <ReplayWhiteboard classroomReplayStore={classroomReplayStore} />
@@ -50,21 +69,7 @@ export const ReplayPage = observer<ReplayPageProps>(function ReplayPage({ match 
                     isShow
                     isVideoOn
                     chatSlot={<ChatPanelReplay classRoomReplayStore={classroomReplayStore} />}
-                    videoSlot={
-                        <div className="replay-videos">
-                            <ReplayVideo
-                                classroomReplayStore={classroomReplayStore}
-                                user={classroomReplayStore.users.creator}
-                            />
-                            {classroomReplayStore.onStageUserUUIDs.map(uuid => (
-                                <ReplayVideo
-                                    key={uuid}
-                                    classroomReplayStore={classroomReplayStore}
-                                    video={classroomReplayStore.userVideos.get(uuid)}
-                                />
-                            ))}
-                        </div>
-                    }
+                    videoSlot={!isSmallClass && ReplayVideos}
                 />
             </div>
         </div>
@@ -72,34 +77,3 @@ export const ReplayPage = observer<ReplayPageProps>(function ReplayPage({ match 
 });
 
 export default ReplayPage;
-
-export interface ReplayVideoProps {
-    classroomReplayStore: ClassroomReplayStore;
-    user?: User | null | undefined;
-    video?: HTMLVideoElement | undefined;
-}
-
-const ReplayVideo = observer<ReplayVideoProps>(function ReplayVideo({
-    classroomReplayStore,
-    user,
-    video: realVideo,
-}) {
-    const ref = useRef<HTMLDivElement>(null);
-    const video = useMemo(
-        () => realVideo || (user && classroomReplayStore.userVideos.get(user.userUUID)),
-        [classroomReplayStore.userVideos, user, realVideo],
-    );
-
-    useEffect(() => {
-        if (ref.current) {
-            while (ref.current.firstChild) {
-                ref.current.removeChild(ref.current.lastChild!);
-            }
-            if (video) {
-                ref.current.appendChild(video);
-            }
-        }
-    }, [video]);
-
-    return <div ref={ref} className="replay-video" />;
-});
