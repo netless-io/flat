316f21819229377c6eb13ff02592447816ca5dc7
Leo <1826234293@qq.com>
2022-11-22 16:59:13 +0800

    fix(flat-components): add cases where the backend does not return error messages (#1788)

--- packages/flat-components/src/utils/errorTip.ts
+++ packages/flat-components/src/utils/errorTip.ts
@@ -8,12 +8,21 @@ export const errorTips = (e: unknown): void => {
     }
 
     if (isServerRequestError(e)) {
-        void message.error({
-            content: FlatI18n.t(e.errorMessage),
-            key: e.errorMessage,
-        });
+        if (e.errorMessage) {
+            void message.error({
+                content: FlatI18n.t(e.errorMessage),
+                key: e.errorCode,
+            });
+        } else {
+            void message.error({
+                content: FlatI18n.t("error-code-error", { code: `${e.errorCode}` }),
+                key: e.errorCode,
+            });
+        }
+    } else if ((e as Error).message) {
+        const { message: content, message: key } = e as Error;
+        void message.error({ content, key });
+    } else {
+        void message.error({ content: FlatI18n.t("unknown-error"), key: "unknown" });
     }
-
-    const { message: content, message: key } = e as Error;
-    void message.error({ content, key });
 };
