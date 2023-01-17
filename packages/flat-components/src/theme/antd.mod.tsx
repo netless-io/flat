f1e16ae9963c77b5718bd668225dc28486aa9c1c
hyrious <hyrious@outlook.com>
2023-01-17 14:32:57 +0800

    refactor(flat-components): use ESM version locale in antd (#1818)

--- packages/flat-components/src/theme/antd.mod.tsx
+++ packages/flat-components/src/theme/antd.mod.tsx
@@ -1,8 +1,8 @@
 import React, { useMemo } from "react";
 
 import { ConfigProvider } from "antd";
-import zhCN from "antd/lib/locale/zh_CN";
-import enUS from "antd/lib/locale/en_US";
+import zhCN from "antd/es/locale/zh_CN";
+import enUS from "antd/es/locale/en_US";
 
 export interface AntdProviderProps {
     lang: string;
