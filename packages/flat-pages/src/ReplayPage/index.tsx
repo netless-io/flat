e15fe9016b9fc7534f321b0c1fb761c069481185
hyrious <hyrious@outlook.com>
2023-04-17 08:59:30 +0800

    fix(flat-pages): register apps in replay page (#1893)

--- packages/flat-pages/src/ReplayPage/index.tsx
+++ packages/flat-pages/src/ReplayPage/index.tsx
@@ -15,6 +15,7 @@ import { useLoginCheck } from "../utils/use-login-check";
 import { ReplayList } from "./ReplayList";
 import { ReplayVideo } from "./ReplayVideo";
 import { ReplayWhiteboard } from "./ReplayWhiteboard";
+import { withFlatServices } from "../components/FlatServicesContext";
 
 export type ReplayPageProps = RouteComponentProps<{
     roomUUID: string;
@@ -22,50 +23,52 @@ export type ReplayPageProps = RouteComponentProps<{
     roomType: RoomType;
 }>;
 
-export const ReplayPage = observer<ReplayPageProps>(function ReplayPage({ match }) {
-    useLoginCheck();
+export const ReplayPage = withFlatServices("whiteboard")(
+    observer<ReplayPageProps>(function ReplayPage({ match }) {
+        useLoginCheck();
 
-    const history = useHistory();
-    const classroomReplayStore = useClassroomReplayStore(match.params);
-    const [showExitReplayModal, setShowExitReplayModal] = useState(false);
+        const history = useHistory();
+        const classroomReplayStore = useClassroomReplayStore(match.params);
+        const [showExitReplayModal, setShowExitReplayModal] = useState(false);
 
-    const windowsBtn = useContext(WindowsSystemBtnContext);
+        const windowsBtn = useContext(WindowsSystemBtnContext);
 
-    if (!classroomReplayStore) {
-        return <LoadingPage />;
-    }
+        if (!classroomReplayStore) {
+            return <LoadingPage />;
+        }
 
-    return (
-        <div className="replay-container">
-            {windowsBtn && (
-                <TopBar
-                    showWindowsSystemBtn={windowsBtn.showWindowsBtn}
-                    onClickWindowsSystemBtn={windowsBtn.onClickWindowsSystemBtn}
-                    onDoubleClick={windowsBtn.clickWindowMaximize}
-                />
-            )}
-            <div className="replay-videos is-horizontal">
-                <ReplayVideo classroomReplayStore={classroomReplayStore} />
-            </div>
-            <div className="replay-content">
-                <ReplayWhiteboard classroomReplayStore={classroomReplayStore} />
-                <div className="replay-share-screen">{/* TODO */}</div>
-                <div
-                    className={classNames("replay-bottom", {
-                        "is-playing": classroomReplayStore.isPlaying,
-                    })}
-                >
-                    <ReplayList classroomReplayStore={classroomReplayStore} />
+        return (
+            <div className="replay-container">
+                {windowsBtn && (
+                    <TopBar
+                        showWindowsSystemBtn={windowsBtn.showWindowsBtn}
+                        onClickWindowsSystemBtn={windowsBtn.onClickWindowsSystemBtn}
+                        onDoubleClick={windowsBtn.clickWindowMaximize}
+                    />
+                )}
+                <div className="replay-videos is-horizontal">
+                    <ReplayVideo classroomReplayStore={classroomReplayStore} />
                 </div>
+                <div className="replay-content">
+                    <ReplayWhiteboard classroomReplayStore={classroomReplayStore} />
+                    <div className="replay-share-screen">{/* TODO */}</div>
+                    <div
+                        className={classNames("replay-bottom", {
+                            "is-playing": classroomReplayStore.isPlaying,
+                        })}
+                    >
+                        <ReplayList classroomReplayStore={classroomReplayStore} />
+                    </div>
+                </div>
+                <ExitReplayConfirm
+                    visible={showExitReplayModal}
+                    onCancel={() => setShowExitReplayModal(false)}
+                    onConfirm={() => history.goBack()}
+                    onSendWindowWillClose={() => setShowExitReplayModal(true)}
+                />
             </div>
-            <ExitReplayConfirm
-                visible={showExitReplayModal}
-                onCancel={() => setShowExitReplayModal(false)}
-                onConfirm={() => history.goBack()}
-                onSendWindowWillClose={() => setShowExitReplayModal(true)}
-            />
-        </div>
-    );
-});
+        );
+    }),
+);
 
 export default ReplayPage;
