f997c07554c758769fe7a04176cf4fc48b8d3279
hyrious <hyrious@outlook.com>
2023-07-26 15:08:19 +0800

    fix(flat-pages): replace loadable component in favor of suspense (#1973)
    - fixes possible page reloading when changed location (pushHistory)
      if the pages components are not completely loaded.

--- packages/flat-pages/src/AppRoutes/AppRouteContainer.tsx
+++ packages/flat-pages/src/AppRoutes/AppRouteContainer.tsx
@@ -1,9 +1,8 @@
-import React, { ComponentType, useContext, useEffect } from "react";
+import React, { ComponentType, useContext, useEffect, lazy, Suspense } from "react";
 import { observer } from "mobx-react-lite";
-import loadable from "@loadable/component";
 import { useTranslate } from "@netless/flat-i18n";
 import { FlatThemeBodyProvider, LoadingPage } from "flat-components";
-import { RouteComponentProps } from "react-router-dom";
+import { Redirect, RouteComponentProps } from "react-router-dom";
 import { useIsomorphicLayoutEffect } from "react-use";
 import { PageStoreContext, PreferencesStoreContext } from "../components/StoreProvider";
 import { RouteNameType } from "../route-config";
@@ -14,27 +13,17 @@ import { WeChatRedirect } from "./WeChatRedirect";
 
 export interface AppRouteContainerProps {
     name: RouteNameType;
-    Comp: () => Promise<{ default: ComponentType<any> }>;
     title: string;
     routeProps: RouteComponentProps;
 }
 
-const componentCache = new Map<RouteNameType, ComponentType<any>>();
-// Preload components every 2 seconds.
-const preloadComponents = async (): Promise<void> => {
-    for (const name of Object.keys(routePages) as RouteNameType[]) {
-        if (!componentCache.has(name)) {
-            const { default: Component } = await routePages[name].component();
-            componentCache.set(name, Component);
-            await new Promise(resolve => setTimeout(resolve, 2000));
-        }
-    }
-};
-window.setTimeout(preloadComponents, 5000);
+const lazyPages: Record<string, ComponentType<any>> = {};
+for (const name of Object.keys(routePages) as RouteNameType[]) {
+    lazyPages[name] = lazy(routePages[name].component);
+}
 
 export const AppRouteContainer = observer<AppRouteContainerProps>(function AppRouteContainer({
     name,
-    Comp,
     title,
     routeProps,
 }) {
@@ -53,14 +42,6 @@ export const AppRouteContainer = observer<AppRouteContainerProps>(function AppRo
         window.getSelection()?.removeAllRanges();
     }, [t, title]);
 
-    useEffect(() => {
-        if (!componentCache.has(name)) {
-            Comp().then(({ default: Component }) => {
-                componentCache.set(name, Component);
-            });
-        }
-    }, [Comp, name]);
-
     const hasHeader =
         pageStore.name !== null && pageStore.name && routePages[pageStore.name].hasHeader;
 
@@ -69,17 +50,19 @@ export const AppRouteContainer = observer<AppRouteContainerProps>(function AppRo
         return <WeChatRedirect />;
     }
 
+    const Page = lazyPages[name];
+    if (!Page) {
+        console.error(`Page not found: ${name}`);
+        return <Redirect to="/" />;
+    }
+
     return (
         <FlatThemeBodyProvider prefersColorScheme={preferencesStore.prefersColorScheme}>
-            <AppRouteErrorBoundary
-                Comp={
-                    componentCache.get(name) ||
-                    loadable(Comp, {
-                        fallback: <LoadingPage hasHeader={hasHeader} />,
-                    })
-                }
-                {...{ title, routeProps }}
-            />
+            <AppRouteErrorBoundary>
+                <Suspense fallback={<LoadingPage hasHeader={hasHeader} />}>
+                    <Page {...routeProps} />
+                </Suspense>
+            </AppRouteErrorBoundary>
         </FlatThemeBodyProvider>
     );
 });
