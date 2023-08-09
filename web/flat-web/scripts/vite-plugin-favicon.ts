f4fd358a29c4e9cd019ac590f218f5f018b7c430
hyrious <hyrious@outlook.com>
2023-08-09 17:09:55 +0800

    fix: handle favicon in build mode

--- web/flat-web/scripts/vite-plugin-favicon.ts
+++ web/flat-web/scripts/vite-plugin-favicon.ts
@@ -1,14 +1,24 @@
 import type { Plugin } from "vite";
 
 export function generateFavicon(): Plugin {
+    function deleteLine(html: string, search: string): string {
+        const lines = html.split("\n");
+        const index = lines.findIndex((line) => line.includes(search));
+        if (index >= 0) {
+            lines.splice(index, 1);
+        }
+        return lines.join("\n");
+    }
+
     return {
         name: "flat:generate-favicon",
         transformIndexHtml(html) {
             const region = process.env.FLAT_REGION || "CN";
-            return html.replace(
-                "DYNAMIC_FAVICON_PATH",
-                region === "CN" ? "/favicon.ico" : "/favicon_en.ico",
-            );
+            if (region === "CN") {
+                return deleteLine(html, 'type="image/x-icon" lang="en"');
+            } else {
+                return deleteLine(html, 'type="image/x-icon"'); // this matches the first line
+            }
         },
     };
 }
