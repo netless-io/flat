b02c1901d78459e4f7c8f540bb6dcb2c16dc44cf
qianzhu <36325249+syt-honey@users.noreply.github.com>
2023-08-18 18:33:08 +0800

    refactor(flat-pages): refactor personal page style (#2010)
    refactor(flat-pages): remove useless console

---
+++ packages/flat-pages/src/UserSettingPage/GeneralSettingPage/BindingField.tsx
@@ -0,0 +1,60 @@
+import { Button } from "antd";
+import React, { useMemo, useState } from "react";
+import { LoginPlatform } from "@netless/flat-server-api";
+
+import closeSVG from "./icons/close.svg";
+
+// binding phone & email field
+interface BindingFieldProps {
+    key: string;
+    msg: string;
+    icon: string;
+    desc: string;
+    payload: { content: string; type: LoginPlatform };
+
+    handleShowModel: () => void;
+    unbind: (content: string, type: LoginPlatform) => Promise<void>;
+}
+
+export function BindingField({
+    key,
+    msg,
+    icon,
+    desc,
+    payload,
+
+    handleShowModel,
+    unbind,
+}: BindingFieldProps): React.ReactElement {
+    const { content, type } = payload;
+
+    const [hovering, setHovering] = useState(false);
+    const hasKey = useMemo(() => !!key, [key]);
+
+    return (
+        <div className="editable-container">
+            <div
+                className={`input-container-bg ${
+                    hovering && hasKey ? "input-container-bg-hover" : ""
+                }`}
+                onMouseEnter={() => setHovering(true)}
+                onMouseLeave={() => setHovering(false)}
+            >
+                <img alt={icon} className="general-setting-item-icon" src={icon} />
+                <span className="general-setting-item-icon-desc">{desc}</span>
+                {hasKey ? (
+                    <span>{key}</span>
+                ) : (
+                    <Button type="link" onClick={handleShowModel}>
+                        {msg}
+                    </Button>
+                )}
+                {hovering && hasKey && (
+                    <Button type="link" onClick={() => unbind(content, type)}>
+                        <img alt="close" src={closeSVG} />
+                    </Button>
+                )}
+            </div>
+        </div>
+    );
+}
