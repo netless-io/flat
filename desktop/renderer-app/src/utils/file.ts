396f169c6f7cf8b5bc8cc5776dc9e529b5de31f8
hyrious <hyrious@outlook.com>
2023-08-21 12:10:20 +0800

    refactor(desktop): cleanup renderer app (#2014)

--- desktop/renderer-app/src/utils/file.ts
+++
@@ -1,7 +0,0 @@
-/**
- * "name.txt" => "txt"
- */
-export const getFileExt = (fileName: string): string =>
-    (/\.([^.]+)$/.exec(fileName) || ["", ""])[1].toLowerCase();
-
-export const isPPTX = (fileName: string): boolean => /\.pptx$/i.test(fileName);
