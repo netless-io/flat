b0f8aa6a942be1f09df90d39ae7dec1966ba48ff
qianzhu <36325249+syt-honey@users.noreply.github.com>
2023-08-21 17:45:15 +0800

    refactor(flat-components): set cover no-draggable

--- packages/flat-components/src/components/LoginPage/icons/Cover.tsx
+++ packages/flat-components/src/components/LoginPage/icons/Cover.tsx
@@ -3,5 +3,5 @@ import coverCNSVG from "./cover.svg";
 import coverENSVG from "./cover-en.svg";
 
 export function Cover({ isZh }: { isZh: boolean }): React.ReactElement {
-    return <img alt="cover" src={isZh ? coverCNSVG : coverENSVG} />;
+    return <img alt="cover" draggable={false} src={isZh ? coverCNSVG : coverENSVG} />;
 }
--- packages/flat-components/src/components/LoginPage/icons/CoverDark.tsx
+++ packages/flat-components/src/components/LoginPage/icons/CoverDark.tsx
@@ -3,5 +3,5 @@ import coverDarkCNSVG from "./cover-dark.svg";
 import coverDarkENSVG from "./cover-dark-en.svg";
 
 export function CoverDark({ isZh }: { isZh: boolean }): React.ReactElement {
-    return <img alt="cover-dark" src={isZh ? coverDarkCNSVG : coverDarkENSVG} />;
+    return <img alt="cover-dark" draggable={false} src={isZh ? coverDarkCNSVG : coverDarkENSVG} />;
 }
