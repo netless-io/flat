1742610d0b5a59b563d3f775f606a68f6d9aff98
hyrious <hyrious@outlook.com>
2023-08-23 14:25:14 +0800

    fix(desktop): open external links in browser

--- desktop/main-app/src/utils/window-event.ts
+++ desktop/main-app/src/utils/window-event.ts
@@ -1,6 +1,7 @@
 import { ipcEmit } from "./ipc-emit";
 import { autoUpdater } from "electron-updater";
 import { CustomWindow } from "../window-manager/abstract";
+import { shell } from "electron";
 
 export const windowHookClose = (customWindow: CustomWindow): void => {
     customWindow.window.on("close", e => {
@@ -46,5 +47,16 @@ export const windowOpenDevTools = (customWindow: CustomWindow): void => {
 
             customWindow.window.webContents.openDevTools();
         }
+    });
+};
+
+export const windowOpenExternalLink = (customWindow: CustomWindow): void => {
+    customWindow.window.webContents.setWindowOpenHandler(details => {
+        if (/^(https?|mailto):/.test(details.url)) {
+            shell.openExternal(details.url);
+            return { action: "deny" };
+        }
+
+        return { action: "allow" };
     });
 };
