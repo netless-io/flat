netless-io/flat

commit ea0c7ffb62dd6f632b3ff3fcc0c7011f05ec8196

refactor(flat-pages): add stop class command in room list (#2052)

packages/flat-pages/src/HomePage/MainRoomListPanel/MainRoomList.tsx
@@ -11,9 +11,10 @@ import {
     RoomListItemPrimaryAction,
     RoomListSkeletons,
     RoomStatusType,
+    StopClassConfirmModal,
     errorTips,
 } from "flat-components";
-import { ListRoomsType, RoomStatus, RoomType } from "@netless/flat-server-api";
+import { ListRoomsType, RoomStatus, RoomType, listPmi, stopClass } from "@netless/flat-server-api";
 import { GlobalStoreContext, RoomStoreContext } from "../../components/StoreProvider";
 import { RoomItem } from "@netless/flat-stores";
 import { useSafePromise } from "../../utils/hooks/lifecycle";
@@ -37,6 +38,7 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
     const [skeletonsVisible, setSkeletonsVisible] = useState(false);
     const [roomUUIDs, setRoomUUIDs] = useState<string[]>();
     const [cancelModalVisible, setCancelModalVisible] = useState(false);
+    const [stopModalVisible, setStopModalVisible] = useState(false);
     const [inviteModalVisible, setInviteModalVisible] = useState(false);
     const [removeHistoryVisible, setRemoveHistoryVisible] = useState(false);
     const [removeHistoryLoading, setRemoveHistoryLoading] = useState(false);
@@ -188,6 +190,11 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
                                         setCancelModalVisible(true);
                                         break;
                                     }
+                                    case "stop": {
+                                        setCurrentRoom(room);
+                                        setStopModalVisible(true);
+                                        break;
+                                    }
                                     case "invite": {
                                         setCurrentRoom(room);
                                         setInviteModalVisible(true);
@@ -239,6 +246,14 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
                 />
             )}
             {currentRoom && (
+                <StopClassConfirmModal
+                    loading={false}
+                    visible={stopModalVisible}
+                    onCancel={hideStopModal}
+                    onStop={stopRoomHandler}
+                />
+            )}
+            {currentRoom && (
                 <InviteModal
                     baseUrl={FLAT_WEB_BASE_URL}
                     periodicWeeks={periodicInfo?.periodic.weeks}
@@ -279,6 +294,10 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
 
     function hideCancelModal(): void {
         setCancelModalVisible(false);
+    }
+
+    function hideStopModal(): void {
+        setStopModalVisible(false);
     }
 
     function hideInviteModal(): void {
@@ -343,6 +362,27 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
         }
     }
 
+    async function stopRoomHandler(): Promise<void> {
+        const { ownerUUID, roomUUID, roomStatus } = currentRoom!;
+        const isCreator = ownerUUID === globalStore.userUUID;
+        const isStarted = roomStatus === RoomStatus.Started || roomStatus === RoomStatus.Paused;
+        try {
+            if (isCreator && isStarted) {
+                await stopClass(roomUUID);
+
+                if (globalStore.pmiRoomUUID === roomUUID) {
+                    // remove pmi room id list
+                    globalStore.updatePmiRoomList(await listPmi());
+                }
+                void refreshRooms();
+            }
+            setStopModalVisible(false);
+        } catch (e) {
+            console.error(e);
+            errorTips(e);
+        }
+    }
+
     async function removeConfirm(): Promise<void> {
         setRemoveHistoryLoading(true);
         try {
@@ -364,7 +404,7 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
 
     type SubActions =
         | Array<{ key: "details" | "share" | "delete-history"; text: string }>
-        | Array<{ key: "details" | "modify" | "cancel" | "invite"; text: string }>;
+        | Array<{ key: "details" | "modify" | "cancel" | "stop" | "invite"; text: string }>;
 
     function getSubActions(room: RoomItem): SubActions {
         const result = [{ key: "details", text: t("room-detail") }];

packages/flat-stores/src/global-store.ts
@@ -125,8 +125,8 @@ export class GlobalStore {
         this.pmi = pmi;
     };
 
-    public updatePmiRoomList = (pmiRoomList: PmiRoom[]): void => {
-        this.pmiRoomList = pmiRoomList;
+    public updatePmiRoomList = (pmiRoomList?: PmiRoom[]): void => {
+        this.pmiRoomList = pmiRoomList || [];
     };
 
     public updateUserInfo = (userInfo: UserInfo | null): void => {

packages/flat-pages/src/HomePage/MainRoomMenu/index.tsx
@@ -58,7 +58,7 @@ export const MainRoomMenu: FC = () => {
 
             if (pmi) {
                 // update pmi room list
-                globalStore.updatePmiRoomList((await listPmi()) || []);
+                globalStore.updatePmiRoomList(await listPmi());
             }
 
             await onJoinRoom(roomUUID);

packages/flat-pages/src/LoginPage/utils/state.ts
@@ -88,7 +88,7 @@ export function useLoginState(): LoginState {
         async (authData: LoginProcessResult | null, account?: Account) => {
             globalStore.updateUserInfo(authData);
             globalStore.updatePmi((await createOrGetPmi({ create: true }))?.pmi || null);
-            globalStore.updatePmiRoomList((await listPmi()) || []);
+            globalStore.updatePmiRoomList(await listPmi());
 
             if (!authData) {
                 setLoginResult(null);

packages/flat-stores/src/classroom-store/index.ts
@@ -1373,7 +1373,7 @@ export class ClassroomStore {
 
                     if (globalStore.pmiRoomUUID === this.roomUUID) {
                         // remove pmi room id list
-                        globalStore.updatePmiRoomList((await listPmi()) || []);
+                        globalStore.updatePmiRoomList(await listPmi());
                     }
                     break;
                 }

commit dab89faed43a76bddd810c5fa8bd4b7ce244f37a

fix(flat-pages): add stop class command and refresh list correctly
- fixes a regression introduced in ea0c7ffb

packages/flat-pages/src/HomePage/MainRoomHistoryPanel/index.tsx
@@ -1,21 +1,21 @@
 // import "../MainRoomListPanel/MainRoomList.less";
 
-import React, { useCallback, useContext } from "react";
+import React, { useCallback } from "react";
 import { observer } from "mobx-react-lite";
-import { MainRoomList } from "../MainRoomListPanel/MainRoomList";
 import { ListRoomsType } from "@netless/flat-server-api";
 import { RoomList } from "flat-components";
 import { useTranslate } from "@netless/flat-i18n";
-import { RoomStoreContext } from "../../components/StoreProvider";
+import { RoomStore } from "@netless/flat-stores";
+import { MainRoomList } from "../MainRoomListPanel/MainRoomList";
 
 interface MainRoomHistoryPanelProps {
-    isLogin: boolean;
+    roomStore: RoomStore;
+    refreshRooms: () => Promise<void>;
 }
 
 export const MainRoomHistoryPanel = observer<MainRoomHistoryPanelProps>(
-    function MainRoomHistoryPanel({ isLogin }) {
+    function MainRoomHistoryPanel({ roomStore, refreshRooms }) {
         const t = useTranslate();
-        const roomStore = useContext(RoomStoreContext);
 
         const onScrollToBottom = useCallback((): void => {
             void roomStore.fetchMoreRooms(ListRoomsType.History);
@@ -24,8 +24,8 @@ export const MainRoomHistoryPanel = observer<MainRoomHistoryPanelProps>(
         return (
             <RoomList title={t("history")} onScrollToBottom={onScrollToBottom}>
                 <MainRoomList
-                    isLogin={isLogin}
                     listRoomsType={ListRoomsType.History}
+                    refreshRooms={refreshRooms}
                     roomStore={roomStore}
                 />
             </RoomList>

packages/flat-pages/src/HomePage/MainRoomListPanel/MainRoomList.tsx
@@ -1,5 +1,5 @@
 import { message } from "antd";
-import React, { Fragment, useCallback, useContext, useEffect, useState } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
 import {
     InviteModal,
@@ -27,13 +27,13 @@ import { generateAvatar } from "../../utils/generate-avatar";
 export interface MainRoomListProps {
     roomStore: RoomStore;
     listRoomsType: ListRoomsType;
-    isLogin: boolean;
+    refreshRooms: () => Promise<void>;
 }
 
 export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
     roomStore,
     listRoomsType,
-    isLogin,
+    refreshRooms,
 }) {
     const t = useTranslate();
     const [skeletonsVisible, setSkeletonsVisible] = useState(false);
@@ -53,31 +53,6 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
         const ticket = window.setTimeout(() => setSkeletonsVisible(true), 200);
         return () => window.clearTimeout(ticket);
     }, []);
-
-    const refreshRooms = useCallback(
-        async function refreshRooms(): Promise<void> {
-            try {
-                await sp(roomStore.listRooms(listRoomsType));
-            } catch (e) {
-                errorTips(e);
-            }
-        },
-        [listRoomsType, roomStore, sp],
-    );
-
-    useEffect(() => {
-        if (!isLogin) {
-            return;
-        }
-
-        void refreshRooms();
-
-        const ticket = window.setInterval(refreshRooms, 30 * 1000);
-
-        return () => {
-            window.clearInterval(ticket);
-        };
-    }, [refreshRooms, isLogin]);
 
     const roomUUIDs = roomStore.roomUUIDs[listRoomsType];
 
@@ -429,6 +404,11 @@ export const MainRoomList = observer<MainRoomListProps>(function MainRoomList({
                 result.push({
                     key: "cancel",
                     text: isCreator ? t("cancel-room") : t("remove-room"),
+                });
+            } else if (isCreator) {
+                result.push({
+                    key: "stop",
+                    text: t("end-the-class"),
                 });
             }
             if (room.roomUUID) {

packages/flat-pages/src/HomePage/MainRoomListPanel/index.tsx
@@ -1,16 +1,21 @@
-import React, { useContext, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { RoomList } from "flat-components";
-import { MainRoomList } from "./MainRoomList";
 import { ListRoomsType } from "@netless/flat-server-api";
 import { useTranslate } from "@netless/flat-i18n";
-import { RoomStoreContext } from "../../components/StoreProvider";
+import { RoomStore } from "@netless/flat-stores";
+import { MainRoomList } from "./MainRoomList";
 
-export const MainRoomListPanel = observer<{ isLogin: boolean }>(function MainRoomListPanel({
-    isLogin,
+interface MainRoomListPanelProps {
+    roomStore: RoomStore;
+    refreshRooms: () => Promise<void>;
+}
+
+export const MainRoomListPanel = observer<MainRoomListPanelProps>(function MainRoomListPanel({
+    roomStore,
+    refreshRooms,
 }) {
     const t = useTranslate();
-    const roomStore = useContext(RoomStoreContext);
     const [activeTab, setActiveTab] = useState<"all" | "today" | "periodic">("all");
     const filters = useMemo<Array<{ key: "all" | "today" | "periodic"; title: string }>>(
         () => [
@@ -38,8 +43,8 @@ export const MainRoomListPanel = observer<{ isLogin: boolean }>(function MainRoo
             onTabActive={setActiveTab}
         >
             <MainRoomList
-                isLogin={isLogin}
                 listRoomsType={activeTab as ListRoomsType}
+                refreshRooms={refreshRooms}
                 roomStore={roomStore}
             />
         </RoomList>

packages/flat-pages/src/HomePage/index.tsx
@@ -1,28 +1,62 @@
 import "./style.less";
 
-import React, { useContext, useEffect } from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import { observer } from "mobx-react-lite";
+import { ListRoomsType } from "@netless/flat-server-api";
+import { errorTips, useSafePromise } from "flat-components";
 import { MainRoomMenu } from "./MainRoomMenu";
 import { MainRoomListPanel } from "./MainRoomListPanel";
 import { MainRoomHistoryPanel } from "./MainRoomHistoryPanel";
 import { useLoginCheck } from "../utils/use-login-check";
-import { PageStoreContext } from "../components/StoreProvider";
+import { PageStoreContext, RoomStoreContext } from "../components/StoreProvider";
 import { AppUpgradeModal } from "../components/AppUpgradeModal";
 
 export const HomePage = observer(function HomePage() {
+    const sp = useSafePromise();
     const pageStore = useContext(PageStoreContext);
+    const roomStore = useContext(RoomStoreContext);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => pageStore.configure(), []);
 
     const isLogin = useLoginCheck();
 
+    // If you stop a class, it will "fly to" the history list,
+    // which means we need 2 refresh list api calls here.
+    const refreshRooms = useCallback(
+        async function refreshRooms() {
+            try {
+                await Promise.all([
+                    sp(roomStore.listRooms(ListRoomsType.All)),
+                    sp(roomStore.listRooms(ListRoomsType.History)),
+                ]);
+            } catch (e) {
+                errorTips(e);
+            }
+        },
+        [roomStore, sp],
+    );
+
+    useEffect(() => {
+        if (!isLogin) {
+            return;
+        }
+
+        void refreshRooms();
+
+        const ticket = window.setInterval(refreshRooms, 30 * 1000);
+
+        return () => {
+            window.clearInterval(ticket);
+        };
+    }, [refreshRooms, isLogin]);
+
     return (
         <div className="homepage-layout-horizontal-container">
             <MainRoomMenu />
             <div className="homepage-layout-horizontal-content">
-                <MainRoomListPanel isLogin={isLogin} />
-                <MainRoomHistoryPanel isLogin={isLogin} />
+                <MainRoomListPanel refreshRooms={refreshRooms} roomStore={roomStore} />
+                <MainRoomHistoryPanel refreshRooms={refreshRooms} roomStore={roomStore} />
             </div>
             <AppUpgradeModal />
         </div>

packages/flat-stores/src/global-store.ts
@@ -6,7 +6,7 @@ import {
     createOrGetPmi,
     listPmi,
 } from "@netless/flat-server-api";
-import { autorun } from "mobx";
+import { autorun, runInAction } from "mobx";
 import { autoPersistStore } from "./utils/auto-persist-store";
 
 // clear storage if not match
@@ -124,11 +124,25 @@ export class GlobalStore {
     }
 
     public updatePmi = async (pmi?: string | null): Promise<void> => {
-        this.pmi = pmi ?? ((await createOrGetPmi({ create: true }))?.pmi || null);
+        if (pmi) {
+            this.pmi = pmi;
+        } else {
+            const pmi = (await createOrGetPmi({ create: true }))?.pmi || null;
+            runInAction(() => {
+                this.pmi = pmi;
+            });
+        }
     };
 
     public updatePmiRoomList = async (pmiRoomList?: PmiRoom[]): Promise<void> => {
-        this.pmiRoomList = pmiRoomList ?? ((await listPmi()) || []);
+        if (pmiRoomList) {
+            this.pmiRoomList = pmiRoomList;
+        } else {
+            const pmiRoomList = (await listPmi()) || [];
+            runInAction(() => {
+                this.pmiRoomList = pmiRoomList;
+            });
+        }
     };
 
     public updateUserInfo = (userInfo: UserInfo | null): void => {

packages/flat-stores/src/room-store.ts
@@ -97,12 +97,12 @@ export class RoomStore {
                 roomUUIDs.history.push(room.roomUUID);
             } else {
                 roomUUIDs.all.push(room.roomUUID);
-            }
-            if (isPeriodic) {
-                roomUUIDs.periodic.push(room.roomUUID);
-            }
-            if (isToday(beginTime)) {
-                roomUUIDs.today.push(room.roomUUID);
+                if (isToday(beginTime)) {
+                    roomUUIDs.today.push(room.roomUUID);
+                }
+                if (isPeriodic) {
+                    roomUUIDs.periodic.push(room.roomUUID);
+                }
             }
         }
         return roomUUIDs;
@@ -206,6 +206,11 @@ export class RoomStore {
 
     public async cancelRoom(payload: CancelRoomPayload): Promise<void> {
         await cancelRoom(payload);
+        runInAction(() => {
+            if (payload.roomUUID) {
+                this.rooms.delete(payload.roomUUID);
+            }
+        });
     }
 
     public async syncOrdinaryRoomInfo(roomUUID: string): Promise<void> {